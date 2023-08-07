f82ff9bf6fd7c947515dc50f9e23b21d924d8584
imnaiyar <137700126+imnaiyar@users.noreply.github.com>
2023-08-07 15:54:33 +0600

    Files

---
+++ interactionhandler/skyTimes.js
@@ -0,0 +1,130 @@
+const moment = require('moment-timezone');
+const { StimesReply } = require('../eventhandler/StimesReply');
+const { PtimesReply } = require('../eventhandler/prefixListener');
+
+async function skyTimes(interaction, message, args) {
+  const targetTimezone = 'Asia/Kolkata'; // Set the target time zone (example: Eastern Time)
+  const now = moment().tz(targetTimezone);
+
+  // Geyser Calculation
+  let geyserResult;
+  const geyserTargetTime = now.clone().startOf('day').add(30, 'minutes');
+
+  while (now.isAfter(geyserTargetTime)) {
+    geyserTargetTime.add(2, 'hours');
+  }
+
+  const geyserUnixTimestamp = Math.floor(geyserTargetTime.valueOf() / 1000);
+  const durationGeyser = moment.duration(geyserTargetTime.diff(now));
+  
+  let geyserResultStr = `<t:${geyserUnixTimestamp}:t> ( in `;
+  if (durationGeyser.hours() > 0) {
+    geyserResultStr += `${durationGeyser.hours()} hours`;
+  }
+  if (durationGeyser.minutes() > 0) {
+    geyserResultStr += ` ${durationGeyser.minutes()} minutes`;
+  }
+  geyserResultStr += ` ${durationGeyser.seconds()} seconds)`;
+
+
+  // Grandma Calculation
+  let grandmaResult;
+  const grandmaTargetTime = now.clone().startOf('day').add(1, 'hours');
+
+  while (now.isAfter(grandmaTargetTime)) {
+    grandmaTargetTime.add(2, 'hours');
+  }
+
+  const grandmaUnixTimestamp = Math.floor(grandmaTargetTime.valueOf() / 1000);
+  const durationGrandma = moment.duration(grandmaTargetTime.diff(now));
+  let grandmaResultStr = `<t:${grandmaUnixTimestamp}:t> ( in `;
+  if (durationGrandma.hours() > 0) {
+    grandmaResultStr += `${durationGrandma.hours()} hours`;
+  }
+  if (durationGrandma.minutes() > 0) {
+    grandmaResultStr += ` ${durationGrandma.minutes()} minutes`;
+  }
+  grandmaResultStr += ` ${durationGrandma.seconds()} seconds)`;
+
+  // Turtle Calculation
+  let turtleResult;
+  const turtleTargetTime = now.clone().startOf('day').add(1, 'hours').add(20, 'minutes');
+
+  while (now.isAfter(turtleTargetTime)) {
+    turtleTargetTime.add(2, 'hours');
+  }
+
+  const turtleUnixTimestamp = Math.floor(turtleTargetTime.valueOf() / 1000);
+  const durationTurtle = moment.duration(turtleTargetTime.diff(now));
+  let turtleResultStr = `<t:${turtleUnixTimestamp}:t> ( in `;
+  if (durationTurtle.hours() > 0) {
+    turtleResultStr += `${durationTurtle.hours()} hours`;
+  }
+  if (durationTurtle.minutes() > 0) {
+    turtleResultStr += ` ${durationTurtle.minutes()} minutes`;
+  }
+  turtleResultStr += ` ${durationTurtle.seconds()} seconds)`;
+
+  // Reset Calculation
+  let resetResult;
+  const resetTargetTime = now.clone().startOf('day').add(12, 'hours').add(30, 'Minutes');
+
+  if (now.isSameOrAfter(resetTargetTime)) {
+    resetTargetTime.add(1, 'days');
+  }
+
+  const resetUnixTimestamp = Math.floor(resetTargetTime.valueOf() / 1000);
+  const durationReset = moment.duration(resetTargetTime.diff(now));
+  let resetResultStr = `<t:${resetUnixTimestamp}:F> ( in `;
+  if (durationReset.hours() > 0) {
+    resetResultStr += `${durationReset.hours()} hours`;
+  }
+  if (durationReset.minutes() > 0) {
+    resetResultStr += ` ${durationReset.minutes()} minutes`;
+  }
+  resetResultStr += ` ${durationReset.seconds()} seconds)`;
+
+
+  // Eden Calculation
+  let edenResult;
+  const targetDayOfWeek = 0; // 0 represents Sunday
+  const currentDayOfWeek = now.day();
+
+  let daysToAdd = targetDayOfWeek - currentDayOfWeek;
+  const edenTargetTime = now.clone().startOf('day').add(daysToAdd, 'days').hour(12).minute(30).second(0);
+  if (daysToAdd <= 0 || (daysToAdd === 0 && now.isAfter(edenTargetTime))) {
+    daysToAdd += 7;
+  }
+
+  if (now.isSameOrAfter(edenTargetTime)) {
+    edenTargetTime.add(7, 'days');
+  }
+
+  const edenUnixTimestamp = Math.floor(edenTargetTime.valueOf() / 1000);
+  const durationEden = moment.duration(edenTargetTime.diff(now));
+  let edenResultStr = `<t:${edenUnixTimestamp}:F> ( in `;
+  if (durationEden.days() > 0) {
+  edenResultStr += `${durationEden.days()} days`;
+}
+if (durationEden.hours() > 0) {
+  edenResultStr += ` ${durationEden.hours()} hours`;
+}
+if (durationEden.minutes() > 0) {
+  edenResultStr += ` ${durationEden.minutes()} minutes`;
+}
+edenResultStr += ` ${durationEden.seconds()} seconds)`;
+
+
+  
+  if (message || args) {
+    await PtimesReply(message, args, geyserResult, grandmaResult, resetResult, edenResult, turtleResult);}
+  else
+  if (interaction) {
+    await StimesReply(interaction, geyserResultStr, grandmaResultStr, resetResultStr, edenResultStr, turtleResultStr);
+  }
+  
+}
+
+module.exports = {
+skyTimes
+};
---
+++ interactionhandler/timestampHandler.js
@@ -0,0 +1,100 @@
+const moment = require("moment-timezone");
+function isTimezoneValid(timezone) {
+  return moment.tz.zone(timezone) !== null;
+};
+function isTimeStringValid(timeString) {
+  const timeRegex = /^(0[0-9]|1[0-9]|2[0-3])\s([0-5][0-9])\s([0-5][0-9])$/;
+  return timeRegex.test(timeString);
+}
+function timestampInteraction(interaction) {
+  const { options } = interaction;
+
+  const timeString = options.getString("time");
+  if (!isTimeStringValid(timeString)) {
+    interaction.reply({ content: `Invalid time format. Please provide time in \`HH mm ss\` format.`, ephemeral: true });
+    return;
+  }
+  const formattedTimeString = `\`${timeString}\``; // Add backticks to escape the time string
+
+  let timezone = options.getString("timezone");
+  if (!timezone) {
+    timezone = "America/Los_Angeles"; // Set default timezone to Los Angeles
+  } else if (!isTimezoneValid(timezone)) {
+    interaction.reply({ content: "Invalid timezone. Please provide a correct one. Use the format: `Continent/City`\nYou can use this website to get your timezone if you don't know it already -> https://timezonedb.com/time-zones", ephemeral: true});
+    return;
+  }
+  const date = options.getInteger("date");
+  const month = options.getString("month");
+  const year = options.getInteger("year");
+
+  const currentDate = new Date();
+  const currentYear = year || currentDate.getFullYear();
+  const currentMonth = currentDate.getMonth() + 1;
+  const currentDateValue = date || currentDate.getDate();
+const dateValue = date || currentDateValue;
+  const monthValue = month || currentMonth;
+  const yearValue = year || currentYear;
+  console.log('Date, Month and Year:', dateValue, monthValue, yearValue)
+    timestamp = moment
+      .tz(
+        `${dateValue} ${monthValue} ${yearValue} ${formattedTimeString}`,
+        "DD MM YYYY HH mm ss",  timezone
+      )
+      .valueOf();
+
+
+  const utcTimestamp = moment.utc(timestamp).valueOf();
+  const offset = moment.tz(timezone).utcOffset();
+  const offsetHours = Math.abs(Math.floor(offset / 60));
+  const offsetMinutes = Math.abs(offset % 60);
+  const offsetString = `UTC${offset >= 0 ? "+" : "-" }${offsetHours
+    .toString()
+    .padStart(2, "0")}:${offsetMinutes.toString().padStart(2, "0")}`;
+
+  let result = "";
+  const date1 = `<t:${Math.floor(utcTimestamp / 1000)}:d>`;
+  const date2 = `<t:${Math.floor(utcTimestamp / 1000)}:D>`;
+  const shortTime = `<t:${Math.floor(utcTimestamp / 1000)}:t>`;
+  const longTime = `<t:${Math.floor(utcTimestamp / 1000)}:T>`;
+  const shortDateAndTime = `<t:${Math.floor(utcTimestamp / 1000)}:f>`;
+  const longDateAndTime = `<t:${Math.floor(utcTimestamp / 1000)}:F>`;
+  const minutes = `<t:${Math.floor(utcTimestamp / 1000)}:R>`;
+
+  const format = options.getString("format");
+
+  if (!date || !month && !format) {
+    result = `Short Time - ${shortTime}\nLong Time - ${longTime}\n`;
+  } else if (date && month && year && !format) {
+    result = `Date (1) - ${date1}\nDate (2) - ${date2}\nShort Time - ${shortTime}\nLong Time - ${longTime}\nShort Date and Time - ${shortDateAndTime}\nLong Date and Time - ${longDateAndTime}\nMinutes - ${minutes}\n`;
+  } else {
+    switch (format) {
+      case "date1":
+        result = `Date (1) - ${date1}`;
+        break;
+      case "date2":
+        result = `Date (2) - ${date2}`;
+        break;
+      case "shortTime":
+        result = `Short Time - ${shortTime}`;
+        break;
+      case "longTime":
+        result = `Long Time - ${longTime}`;
+        break;
+      case "shortDateAndTime":
+        result = `Short Date and Time - ${shortDateAndTime}`;
+        break;
+      case "longDateAndTime":
+        result = `Long Date and Time - ${longDateAndTime}`;
+        break;
+      case "minutes":
+        result = `Minutes - ${minutes}`;
+        break;
+    }
+  }
+
+  result += `\Timezone - \`${timezone}\`\nUTC Offset - \`${offsetString}\``;
+
+  interaction.reply({ content: result });
+}
+
+module.exports = {timestampInteraction}
