65a75bdfb1794a310a5485b1e71f1035410e7dd6
Naiyar <imamnaiyar29@gmail.com>
2023-09-21 04:06:17 +0600

    m

--- src/events/guild/guildCreate.js
+++ src/events/guild/guildCreate.js
@@ -27,6 +27,7 @@ module.exports = async (client, guild) => {
   const owner = guild.members.cache.get(guild.ownerId)
   owner.user.send(`An attrmpt to invite me to your server was made, your server is blacklisted from inviting me for the reason \` ${data.Reason} \`. For that, I've left the server. If you think this is a mistake, you can appeal by joining our support server [here](${config.Support}).`)
   await guild.leave();
+  return;
 }
    const fetchedLogs = await guild.fetchAuditLogs({
 	type: AuditLogEvent.BotAdd,
