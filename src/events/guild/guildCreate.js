d75fa420a8711f157a9997474599a099f9fd7e8a
Naiyar <imamnaiyar29@gmail.com>
2023-09-21 03:00:58 +0600

    test

--- src/events/guild/guildCreate.js
+++ src/events/guild/guildCreate.js
@@ -23,6 +23,7 @@ module.exports = async (client, guild) => {
 // Check if joined guild is blacklisted
  let data = await Guild.findOne({ Guild: guild.id}).catch((err) => {}); 
    if (data) {
+    await guild.leave();
    const fetchedLogs = await guild.fetchAuditLogs({
 	type: AuditLogEvent.BotAdd,
 	limit: 1,
@@ -30,7 +31,6 @@ module.exports = async (client, guild) => {
 
 const firstEntry = fetchedLogs.entries.first();
 firstEntry.executor.send(`The server you invited me to is blacklisted for the reason \` ${data.Reason} \`. For that, I've left the server. If you think this is a mistake, you can appeal by joining our support server [here](${config.Support}).`)
-await guild.leave();
 const embed = new EmbedBuilder()
        .setAuthor({ name: `Blacklisted Server`})
        .setDescription(`${firstEntry.executor.username} tried to invite me to a blacklisted server. I have left the server.`)
