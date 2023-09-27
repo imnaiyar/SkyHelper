1465b26657e1bc27a20ed3377ed2971cf5b907cb
Naiyar <imamnaiyar29@gmail.com>
2023-09-28 02:27:59 +0600

    some changez

--- website/timestampHtml.js
+++ website/timestampHtml.js
@@ -1,7 +1,7 @@
 module.exports = (interaction, fieldsData, unixTime, offset, timezone) => {
   return {
     Content: `<html>
-  <head>
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta property="og:type" content="website">
@@ -89,208 +89,6 @@ module.exports = (interaction, fieldsData, unixTime, offset, timezone) => {
      <span>e</span>
      <span>r</span>
    </div> 
-  
-   <style> 
-     @import url("https://fonts.googleapis.com/css?family=Dosis"); 
-  
-     .word { 
-       font-family: "Dosis", sans-serif; 
-       perspective: 1000px; 
-     } 
-  
-     .word span { 
-       cursor: pointer; 
-       display: inline-block; 
-       font-size: 25px; 
-       user-select: none; 
-       line-height: 0.8; 
-     } 
-  
-     .word span:nth-child(1).active { 
-       animation: balance 1.5s ease-out; 
-       transform-origin: bottom left; 
-     } 
-  
-     @keyframes balance { 
-       0%, 
-       100% { 
-         transform: rotate(0deg); 
-       } 
-  
-       30%, 
-       60% { 
-         transform: rotate(-45deg); 
-       } 
-     } 
-  
-     .word span:nth-child(2).active { 
-       animation: shrinkjump 1s ease-in-out; 
-       transform-origin: bottom center; 
-     } 
-  
-     @keyframes shrinkjump { 
-       10%, 
-       35% { 
-         transform: scale(2, 0.2) translate(0, 0); 
-       } 
-  
-       45%, 
-       50% { 
-         transform: scale(1) translate(0, -150px); 
-       } 
-  
-       80% { 
-         transform: scale(1) translate(0, 0); 
-       } 
-     } 
-  
-     .word span:nth-child(3).active { 
-       animation: falling 2s ease-out; 
-       transform-origin: bottom center; 
-     } 
-  
-     @keyframes falling { 
-       12% { 
-         transform: rotateX(240deg); 
-       } 
-  
-       24% { 
-         transform: rotateX(150deg); 
-       } 
-  
-       36% { 
-         transform: rotateX(200deg); 
-       } 
-  
-       48% { 
-         transform: rotateX(175deg); 
-       } 
-  
-       60%, 
-       85% { 
-         transform: rotateX(180deg); 
-       } 
-  
-       100% { 
-         transform: rotateX(0deg); 
-       } 
-     } 
-  
-     .word span:nth-child(4).active { 
-       animation: rotate 1s ease-out; 
-     } 
-  
-     @keyframes rotate { 
-       20%, 
-       80% { 
-         transform: rotateY(180deg); 
-       } 
-  
-       100% { 
-         transform: rotateY(360deg); 
-       } 
-     } 
-  
-     .word span:nth-child(5).active { 
-       animation: toplong 1.5s linear; 
-     } 
-  
-     @keyframes toplong { 
-       10%, 
-       40% { 
-         transform: translateY(-48vh) scaleY(1); 
-       } 
-  
-       90% { 
-         transform: translateY(-48vh) scaleY(4); 
-       } 
-     } 
-  
-     .word span:nth-child(6).active { 
-       animation: shrinkjump 1s ease-in-out; 
-       transform-origin: bottom center; 
-     } 
-  
-     @keyframes shrinkjump { 
-       10%, 
-       35% { 
-         transform: scale(2, 0.2) translate(0, 0); 
-       } 
-  
-       45%, 
-       50% { 
-         transform: scale(1) translate(0, -150px); 
-       } 
-  
-       80% { 
-         transform: scale(1) translate(0, 0); 
-       } 
-     } 
-  
-     .word span:nth-child(7).active { 
-       animation: balance 1.5s ease-out; 
-       transform-origin: bottom left; 
-     } 
-  
-     @keyframes balance { 
-       0%, 
-       100% { 
-         transform: rotate(0deg); 
-       } 
-  
-       30%, 
-       60% { 
-         transform: rotate(-45deg); 
-       } 
-     }
-     .word span:nth-child(8).active { 
-       animation: rotate 1s ease-out; 
-     } 
-  
-     @keyframes rotate { 
-       20%, 
-       80% { 
-         transform: rotateY(180deg); 
-       } 
-  
-       100% { 
-         transform: rotateY(360deg); 
-       } 
-     } 
-     .word span:nth-child(9).active { 
-       animation: balance 1.5s ease-out; 
-       transform-origin: bottom left; 
-     } 
-  
-     @keyframes balance { 
-       0%, 
-       100% { 
-         transform: rotate(0deg); 
-       } 
-  
-       30%, 
-       60% { 
-         transform: rotate(-45deg); 
-       } 
-     } 
-   </style> 
-  
-   <script> 
-     const spans = document.querySelectorAll(".word span"); 
-  
-     spans.forEach((span, idx) => { 
-       span.addEventListener("click", (e) => { 
-         e.target.classList.add("active"); 
-       }); 
-       span.addEventListener("animationend", (e) => { 
-         e.target.classList.remove("active"); 
-       }); 
-  
-       setTimeout(() => { 
-         span.classList.add("active"); 
-       }, 750 * (idx + 1)); 
-     }); 
-   </script>
 
           <!-- or if you want to use image -->
         </a>
@@ -322,8 +120,8 @@ module.exports = (interaction, fieldsData, unixTime, offset, timezone) => {
             Stats
           </a>
 
-          <a href="pages/commands.html" class="navbar-item is-tab">
-            Docs
+          <a href="https://docs.skyhelper.xyz" class="navbar-item is-tab">
+            Commads
           </a>
         </div>
 
@@ -332,7 +130,7 @@ module.exports = (interaction, fieldsData, unixTime, offset, timezone) => {
             <i class="fa-brands fa-discord"></i>
           </a>
 
-          <a href="https://github.com/imnaiyar/SkyBot.git" class="navbar-item is-tab" target="_blank">
+          <a href="https://github.com/imnaiyar/SkyHelper.git" class="navbar-item is-tab" target="_blank">
             <i class="fa-brands fa-github"></i>
           </a>
 
@@ -417,7 +215,7 @@ var options = { timeZone: '${timezone}' };
         </div>
 
         <p>
-          <span class="has-text-weight-bold">[SkyBot]</span>
+          <span class="has-text-weight-bold">[SkyHelper]</span>
           <br />
           &copy; <span id="cp-year"></span> Copyright NyR. All Rights
           Reserved. Our <a
--- website/views/page/assets/js/script.js
+++ website/views/page/assets/js/script.js
@@ -6,6 +6,21 @@ AOS.init({
   duration: 700,
   once: true
 });
+// Bot's name animation
+const spans = document.querySelectorAll(".word span"); 
+  
+     spans.forEach((span, idx) => { 
+       span.addEventListener("click", (e) => { 
+         e.target.classList.add("active"); 
+       }); 
+       span.addEventListener("animationend", (e) => { 
+         e.target.classList.remove("active"); 
+       }); 
+  
+       setTimeout(() => { 
+         span.classList.add("active"); 
+       }, 750 * (idx + 1)); 
+     }); 
 
 // navbar burger
 document.addEventListener('DOMContentLoaded', () => {
