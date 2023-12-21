98a03991325de212ed73e3596de6d82fa9e45f8a
Naiyar <imamnaiyar29@gmail.com>
2023-12-21 23:04:50 +0600

    test

--- src/commands/prefix/listlive.js
+++ src/commands/prefix/listlive.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { EmbedBuilder } = require('discord.js');
+
 module.exports = {
   data: {
     name: 'listlive',
@@ -9,24 +10,36 @@ module.exports = {
   async execute(msg, args) {
     const model = mongoose.model('autoShard');
     let description = ``;
-    await model.find().then((data) => {
+
+    try {
+      const data = await model.find();
+
       if (data.length === 0) {
         return msg.reply('No active live shards');
       }
-      data.forEach((g) => {
+
+      for (const g of data) {
         const guild = msg.client.guilds.cache.get(g._id);
+
+        if (!guild) continue;
+
         const owner = msg.client.users.cache.get(guild.ownerId);
         const channel = msg.client.channels.cache.get(g.channelId);
         
         console.log("Guilds:", guild.name);
         description += `**Guild:** ${guild?.name || "Unknown"} (${guild?.id || "Unknown"})\n**Owner:** ${owner?.username || "Unknown"} (${owner?.id || "Unknown"})\n**Channel:** ${channel.name} (${channel.id})\n\n`;
-      });
+      }
+
       const embed = new EmbedBuilder()
         .setTitle('Guilds with active Live Shard')
         .setDescription(description);
+
       msg.reply({
         embeds: [embed],
       });
-    });
+    } catch (error) {
+      console.error(error);
+      msg.reply('An error occurred while fetching live shards.');
+    }
   },
 };
