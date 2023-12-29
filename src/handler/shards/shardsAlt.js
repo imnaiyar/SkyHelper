075fd56ec128d29d62566ff09d3ef9646fb6b44e
Naiyar <imamnaiyar29@gmail.com>
2023-12-30 04:12:00 +0600

    code simplification, 'un-messing' it. LOL

--- src/handler/shards/shardsAlt.js
+++ src/handler/shards/shardsAlt.js
@@ -1,6 +1,7 @@
 const moment = require('moment');
 const shardsTime = require('./sub/eventTimings.js');
 const { shardsIndex, getSuffix } = require('@functions/shardsUtil');
+
 async function shardsAlt(currentDate) {
   const timezone = 'America/Los_Angeles';
   const dayOfWeek = currentDate.day();
@@ -8,72 +9,44 @@ async function shardsAlt(currentDate) {
   const today = moment().tz(timezone).startOf('day');
   const noShard = currentDate.isSame(today, 'day') ? 'Today' : `${formatDate}`;
 
-  // get the shard and realm index
   const { currentShard, currentRealm } = await shardsIndex(currentDate);
-
-  // Extracting the shards timings
   const timings = shardsTime(currentDate);
-  let eventTimings;
-  if (currentShard === 'A') {
-    eventTimings = timings.A;
-  } else if (currentShard === 'B') {
-    eventTimings = timings.B;
-  } else if (currentShard === 'C') {
-    eventTimings = timings.C;
-  } else if (currentShard === 'a') {
-    eventTimings = timings.a;
-  } else if (currentShard === 'b') {
-    eventTimings = timings.b;
-  }
+  const eventTimings = timings[currentShard];
 
-  // Defining durations of shards timings
   let eventStatus = '';
   let timeRemaining = '';
-  let lastEventEndTime = eventTimings[eventTimings.length - 1].end;
+  const present = moment().tz(timezone);
 
   for (let i = 0; i < eventTimings.length; i++) {
     const eventTiming = eventTimings[i];
-    const present = moment().tz(timezone);
+    const startUnix = Math.floor(eventTiming.start.valueOf() / 1000);
+    const endUnix = Math.floor(eventTiming.end.valueOf() / 1000);
 
     if (present.isBetween(eventTiming.start, eventTiming.end)) {
-      const endUnix = Math.floor(eventTiming.end.valueOf() / 1000);
-      eventStatus = `${i + 1}${getSuffix(i + 1)} Shard is active right now`;
       const duration = moment.duration(eventTiming.end.diff(present));
+      eventStatus = `${i + 1}${getSuffix(i + 1)} Shard is active right now`;
       timeRemaining = `Ends in ${duration.hours()}h ${duration.minutes()}m ${duration.seconds()}s (at <t:${endUnix}:t>)`;
       break;
     } else if (present.isBefore(eventTiming.start)) {
-      const startUnix = Math.floor(eventTiming.start.valueOf() / 1000);
-      eventStatus = `${i + 1}${getSuffix(i + 1)} Shard has not fallen yet`;
       const duration = moment.duration(eventTiming.start.diff(present));
       const hoursRemaining = Math.floor(duration.asHours());
       const minutesRemaining = Math.floor(duration.asMinutes()) % 60;
       const secondsRemaining = Math.floor(duration.asSeconds()) % 60;
+      eventStatus = `${i + 1}${getSuffix(i + 1)} Shard has not fallen yet`;
       timeRemaining = `Falls in ${hoursRemaining}h ${minutesRemaining}m ${secondsRemaining}s (at <t:${startUnix}:T>)`;
       break;
-    } else if (
-      i < eventTimings.length - 1 &&
-      present.isAfter(eventTiming.end) &&
-      present.isBefore(eventTimings[i + 1].start)
-    ) {
+    } else if (i < eventTimings.length - 1 && present.isAfter(eventTiming.end) && present.isBefore(eventTimings[i + 1].start)) {
       const startUnix2 = Math.floor(eventTimings[i + 1].start.valueOf() / 1000);
-      const endUnix3 = Math.floor(eventTiming.end.valueOf() / 1000);
-      eventStatus = `${i + 1}${getSuffix(
-        i + 1,
-      )} shard ended at <t:${endUnix3}:t>, ${i + 2}${getSuffix(
-        i + 2,
-      )} shard has not fallen yet`;
+      eventStatus = `${i + 1}${getSuffix(i + 1)} shard ended at <t:${endUnix}:t>, ${i + 2}${getSuffix(i + 2)} Shard has not fallen yet`;
       const duration = moment.duration(eventTimings[i + 1].start.diff(present));
       timeRemaining = `Falls in ${duration.hours()}h ${duration.minutes()}m ${duration.seconds()}s (at <t:${startUnix2}:T>)`;
       break;
-    } else if (
-      i === eventTimings.length - 1 &&
-      present.isAfter(eventTiming.end)
-    ) {
-      const unixEnd2 = Math.floor(lastEventEndTime.valueOf() / 1000);
+    } else if (i === eventTimings.length - 1 && present.isAfter(eventTiming.end)) {
+      const unixEnd2 = Math.floor(eventTimings[eventTimings.length - 1].end.valueOf() / 1000);
       eventStatus = `All Shards ended for ${noShard}`;
       timeRemaining = '';
 
-      const duration = moment.duration(present.diff(lastEventEndTime));
+      const duration = moment.duration(present.diff(eventTimings[eventTimings.length - 1].end));
       const hoursRemaining = Math.floor(duration.asHours());
       const minutesRemaining = Math.floor(duration.asMinutes()) % 60;
       const secondsRemaining = Math.floor(duration.asSeconds()) % 60;
@@ -81,6 +54,7 @@ async function shardsAlt(currentDate) {
       break;
     }
   }
+
   return {
     formatDate,
     eventStatus,
--- src/handler/shards/sub/shardsReply.js
+++ src/handler/shards/sub/shardsReply.js
@@ -1,4 +1,5 @@
 const { shardsAlt } = require('@shards/shardsAlt');
+
 async function shardsReply(currentDate) {
   const {
     eventStatus,
@@ -8,178 +9,69 @@ async function shardsReply(currentDate) {
     dayOfWeek,
     noShard,
   } = await shardsAlt(currentDate);
+
   let type = 'Red Shard';
   let location;
   let rewards;
-  let colors = '#FF0000';
+  let colors;
   let showButtons = true;
   let thumbUrl = '';
 
-  if (currentRealm === 'prairie') {
-    if (currentShard === 'a' && ![6, 0].includes(dayOfWeek)) {
-      type = 'Black Shard';
-      location = `Butterfly Fields, Daylight Prairie`;
-      colors = '#000000';
-      rewards = '200 Wax <:wax:1125091974869946369>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226741731119165/ButterflyFields.png';
-    } else if (currentShard === 'A' && ![2, 3].includes(dayOfWeek)) {
-      location = `Bird's Nest, Daylight Prairie`;
-      rewards = '2.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226742024708187/BirdsNest.png';
-    } else if (currentShard === 'b' && ![0, 1].includes(dayOfWeek)) {
-      type = 'Black Shard';
-      colors = '#000000';
-      location = 'Village Island, Daylight Prairie';
-      rewards = '200 Wax <:wax:1125091974869946369>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226741412343989/VillageIsland.png';
-    } else if (currentShard === 'B' && ![3, 4].includes(dayOfWeek)) {
-      location = ' Sanctuary Island, Daylight Prairie';
-      rewards = '3.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226740741263510/SanctuaryIsland.png';
-    } else if (currentShard === 'C' && ![1, 2].includes(dayOfWeek)) {
-      location = 'Prairie Caves, Daylight Prairie';
-      rewards = '2 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226741110349925/PrairieCave.png';
-    } else {
-      showButtons = false;
-    }
-  } else if (currentRealm === 'forest') {
-    if (currentShard === 'a' && ![6, 0].includes(dayOfWeek)) {
-      type = 'Black Shard';
-      location = 'Forest Brook, Hidden Forest';
-      colors = '#000000';
-      rewards = '200 Wax <:wax:1125091974869946369>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226847062671400/ForestBrook.png';
-    } else if (currentShard === 'A' && ![2, 3].includes(dayOfWeek)) {
-      location = 'Treehouse, Hidden Forest';
-      rewards = '3.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226739713650788/Treehouse.png';
-    } else if (currentShard === 'b' && ![0, 1].includes(dayOfWeek)) {
-      type = 'Black Shard';
-      location = 'Boneyard, Hidden Forest';
-      colors = '#000000';
-      rewards = '200 Wax <:wax:1125091974869946369>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226740472819793/Boneyard.png';
-    } else if (currentShard === 'B' && ![3, 4].includes(dayOfWeek)) {
-      location = 'Sunny Forest, Hidden Forest';
-      rewards = '3.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226659518558298/ElevatedClearing.png';
-    } else if (currentShard === 'C' && ![1, 2].includes(dayOfWeek)) {
-      location = 'Forest End, Hidden Forest';
-      rewards = '2.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226740099534868/ForestEnd.png';
-    } else {
-      showButtons = false;
-    }
-  } else if (currentRealm === 'valley') {
-    if (currentShard === 'a' && ![6, 0].includes(dayOfWeek)) {
-      type = 'Black Shard';
-      location = 'Ice Rink, Valley of Triumph';
-      colors = '#000000';
-      rewards = '200 Wax <:wax:1125091974869946369>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226659262693446/IceRink.png';
-    } else if (currentShard === 'A' && ![2, 3].includes(dayOfWeek)) {
-      location = 'Village of Dreams, Valley of Triumph';
-      rewards = '2.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226658918780948/VillageOfDreams.png';
-    } else if (currentShard === 'b' && ![0, 1].includes(dayOfWeek)) {
-      type = 'Black Shard';
-      location = 'Ice Rink, Valley of Triumph';
-      colors = '#000000';
-      rewards = '200 Wax <:wax:1125091974869946369>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226659262693446/IceRink.png';
-    } else if (currentShard === 'B' && ![3, 4].includes(dayOfWeek)) {
-      location = `Hermit's Valley, Valley of Triumph`;
-      rewards = '3.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226658629365770/HermitsValley.png';
-    } else if (currentShard === 'C' && ![1, 2].includes(dayOfWeek)) {
-      location = 'Village of Dreams, Valley of Triumph';
-      rewards = '2.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226658918780948/VillageOfDreams.png';
-    } else {
-      showButtons = false;
-    }
-  } else if (currentRealm === 'wasteland') {
-    if (currentShard === 'a' && ![6, 0].includes(dayOfWeek)) {
-      type = 'Black Shard';
-      location = 'Broken Temple, Golden Wasteland';
-      colors = '#000000';
-      rewards = '200 Wax <:wax:1125091974869946369>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226658209923202/BrokenTemple.png';
-    } else if (currentShard === 'A' && ![2, 3].includes(dayOfWeek)) {
-      location = 'Crab Fields, Golden Wasteland';
-      rewards = '2.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226846794231919/CrabFields.png';
-    } else if (currentShard === 'b' && ![0, 1].includes(dayOfWeek)) {
-      location = 'Battlefield, Golden Wasteland';
-      type = 'Black Shard';
-      colors = '#000000';
-      rewards = '200 Wax <:wax:1125091974869946369>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226657148784680/Battlefield.png';
-    } else if (currentShard === 'B' && ![3, 4].includes(dayOfWeek)) {
-      location = 'Forgotten Ark, Golden Wasteland';
-      rewards = '3.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226657933119508/ForgottenArk.png';
-    } else if (currentShard === 'C' && ![1, 2].includes(dayOfWeek)) {
-      location = 'Graveyard, Golden Wasteland';
-      rewards = '2 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226742263787623/Graveyard.png';
-    } else {
-      showButtons = false;
-    }
-  } else if (currentRealm === 'vault') {
-    if (currentShard === 'a' && ![6, 0].includes(dayOfWeek)) {
-      location = 'Starlight Desert, Vault of Knowledge';
-      type = 'Black Shard';
-      colors = '#000000';
-      rewards = '200 Wax <:wax:1125091974869946369>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226657656287392/StarlightDesert.png';
-    } else if (currentShard === 'A' && ![2, 3].includes(dayOfWeek)) {
-      location = 'Jellyfish Cove, Starlight Desert, Vault of Knowledge';
-      rewards = '3.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226657404616756/JellyfishCove.png';
-    } else if (currentShard === 'b' && ![0, 1].includes(dayOfWeek)) {
-      location = 'Starlight Desert, Vault of Knowledge';
-      type = 'Black Shard';
-      colors = '#000000';
-      rewards = '200 Wax <:wax:1125091974869946369>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226657656287392/StarlightDesert.png';
-    } else if (currentShard === 'B' && ![3, 4].includes(dayOfWeek)) {
-      location = 'Jellyfish Cove, Starlight Desert, Vault of Knowledge';
-      rewards = '3.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226657404616756/JellyfishCove.png';
-    } else if (currentShard === 'C' && ![1, 2].includes(dayOfWeek)) {
-      location = 'Jellyfish Cove, Starlight Desert, Vault of Knowledge';
-      rewards = '3.5 AC <a:ac5:1125338720183267390>';
-      thumbUrl =
-        'https://media.discordapp.net/attachments/867638574571323424/1159226657404616756/JellyfishCove.png';
-    } else {
-      showButtons = false;
-    }
+  const shardConfig = {
+    a: { type: 'Black Shard', colors: '#000000', weekdays: [6, 0] },
+    A: { type: 'Red Shard', colors: '#FF0000', weekdays: [2, 3] },
+    b: { type: 'Black Shard', colors: '#000000', weekdays: [0, 1] },
+    B: { type: 'Red Shard', colors: '#FF0000', weekdays: [3, 4] },
+    C: { type: 'Red Shard', colors: '#FF0000', weekdays: [1, 2] },
+  };
+
+  const realms = {
+    prairie: {
+      a: ['Butterfly Fields, Daylight Prairie', '200 Wax <:wax:1125091974869946369>', 'https://media.discordapp.net/attachments/867638574571323424/1159226741731119165/ButterflyFields.png'],
+      A: ["Bird's Nest, Daylight Prairie", '2.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226742024708187/BirdsNest.png'],
+      b: ['Village Island, Daylight Prairie', '200 Wax <:wax:1125091974869946369>', 'https://media.discordapp.net/attachments/867638574571323424/1159226741412343989/VillageIsland.png'],
+      B: ['Sanctuary Island, Daylight Prairie', '3.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226740741263510/SanctuaryIsland.png'],
+      C: ['Prairie Caves, Daylight Prairie', '2 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226741110349925/PrairieCave.png'],
+    },
+    forest: {
+      a: ['Forest Brook, Hidden Forest', '200 Wax <:wax:1125091974869946369>', 'https://media.discordapp.net/attachments/867638574571323424/1159226847062671400/ForestBrook.png'],
+      A: ['Treehouse, Hidden Forest', '3.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226739713650788/Treehouse.png'],
+      b: ['Boneyard, Hidden Forest', '200 Wax <:wax:1125091974869946369>', 'https://media.discordapp.net/attachments/867638574571323424/1159226740472819793/Boneyard.png'],
+      B: ['Sunny Forest, Hidden Forest', '3.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226659518558298/ElevatedClearing.png'],
+      C: ['Forest End, Hidden Forest', '2.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226740099534868/ForestEnd.png'],
+    },
+    valley: {
+      a: ['Ice Rink, Valley of Triumph', '200 Wax <:wax:1125091974869946369>', 'https://media.discordapp.net/attachments/867638574571323424/1159226659262693446/IceRink.png'],
+      A: ['Village of Dreams, Valley of Triumph', '2.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226658918780948/VillageOfDreams.png'],
+      b: ['Ice Rink, Valley of Triumph', '200 Wax <:wax:1125091974869946369>', 'https://media.discordapp.net/attachments/867638574571323424/1159226659262693446/IceRink.png'],
+      B: ["Hermit's Valley, Valley of Triumph", '3.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226658629365770/HermitsValley.png'],
+      C: ['Village of Dreams, Valley of Triumph', '2.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226658918780948/VillageOfDreams.png'],
+    },
+    wasteland: {
+      a: ['Broken Temple, Golden Wasteland', '200 Wax <:wax:1125091974869946369>', 'https://media.discordapp.net/attachments/867638574571323424/1159226658209923202/BrokenTemple.png'],
+      A: ['Crab Fields, Golden Wasteland', '2.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226846794231919/CrabFields.png'],
+      b: ['Battlefield, Golden Wasteland', '200 Wax <:wax:1125091974869946369>', 'https://media.discordapp.net/attachments/867638574571323424/1159226657148784680/Battlefield.png'],
+      B: ['Forgotten Ark, Golden Wasteland', '3.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226657933119508/ForgottenArk.png'],
+      C: ['Graveyard, Golden Wasteland', '2 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226742263787623/Graveyard.png'],
+    },
+    vault: {
+      a: ['Starlight Desert, Vault of Knowledge', '200 Wax <:wax:1125091974869946369>', 'https://media.discordapp.net/attachments/867638574571323424/1159226657656287392/StarlightDesert.png'],
+      A: ['Jellyfish Cove, Starlight Desert, Vault of Knowledge', '3.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226657404616756/JellyfishCove.png'],
+      b: ['Starlight Desert, Vault of Knowledge', '200 Wax <:wax:1125091974869946369>', 'https://media.discordapp.net/attachments/867638574571323424/1159226657656287392/StarlightDesert.png'],
+      B: ['Jellyfish Cove, Starlight Desert, Vault of Knowledge', '3.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226657404616756/JellyfishCove.png'],
+      C: ['Jellyfish Cove, Starlight Desert, Vault of Knowledge', '3.5 AC <a:ac5:1125338720183267390>', 'https://media.discordapp.net/attachments/867638574571323424/1159226657404616756/JellyfishCove.png'],
+    },
+  };
+
+  const shardInfo = shardConfig[currentShard];
+  const realmInfo = realms[currentRealm];
+
+  if (shardInfo && realmInfo && !shardInfo.weekdays.includes(dayOfWeek)) {
+    type = shardInfo.type;
+    colors = shardInfo.colors;
+    [location, rewards, thumbUrl] = realmInfo[currentShard];
+  } else {
+    showButtons = false;
   }
 
   return {
