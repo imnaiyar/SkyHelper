99f9d64bbb6b8d3294f2b81258d5568e33bafa4b
Naiyar <imamnaiyar29@gmail.com>
2024-01-04 10:03:42 +0600

    small change

--- web/views/assets/js/main.js
+++ web/views/assets/js/main.js
@@ -151,7 +151,7 @@ sr.reveal(`.home__value`, { delay: 700 });
 sr.reveal(`.home__images`, { delay: 800, origin: 'bottom' });
 sr.reveal(`.logos__img`, { interval: 100 });
 sr.reveal(`.value__images, .contact__content`, { origin: 'left' });
-sr.reveal(`.value__content`, { origin: 'right' });
+sr.reveal(`.value__content, .form`, { origin: 'right' });
 
 /* =========== Handle Contact Form Submission */
 $(document).ready(function () {
