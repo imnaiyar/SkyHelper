bc58e5d10888b1fe6da7ffd95d065d6861b0349f
Naiyar <imamnaiyar29@gmail.com>
2024-01-18 03:59:39 +0600

    ss

--- src/events/message/messageCreate.js
+++ src/events/message/messageCreate.js
@@ -14,7 +14,10 @@ const Logger = process.env.COMMANDS_USED
 
 module.exports = async (client, message) => {
   if (message.author.bot) return;
-
+  
+  if (message.mentions.has(client.user) && !OWNER.includes(message.author.id)) {
+    message.channel.send('That\'s me...');
+  }
   // Check Bot'sprefix
   let prefix;
   if (message.content.startsWith(`<@${client.user.id}>`)) {
