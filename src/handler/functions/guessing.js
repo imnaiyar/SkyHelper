7b19163fd76052e3f5775b0967ee3b0085870be3
Naiyar <imamnaiyar29@gmail.com>
2024-01-25 05:39:15 +0600

    mm

--- src/handler/functions/guessing.js
+++ src/handler/functions/guessing.js
@@ -254,6 +254,11 @@ context.fillStyle = '#727272';
 
 context.fillText(member.displayName, canvas.width / 2.5, canvas.height / 1.8);
   
+   context.beginPath();
+  context.arc(canvas.width - 75, canvas.height - 75, 25, 0, Math.PI * 2, true);
+  context.closePath();
+  context.clip();
+  context.drawImage(botAvatar, canvas.width - 100, canvas.height - 100, 50, 50);
   // Draw the smaller circular avatar
 context.beginPath();
 context.arc(125, 125, 75, 0, Math.PI * 2, true);
@@ -261,12 +266,6 @@ context.closePath();
 context.clip();
 context.drawImage(avatar, 25, 25, 200, 200);
 
-// Draw the smaller circular bot avatar
-  context.beginPath();
-  context.arc(canvas.width - 75, canvas.height - 75, 25, 0, Math.PI * 2, true);
-  context.closePath();
-  context.clip();
-  context.drawImage(botAvatar, canvas.width - 100, canvas.height - 100, 50, 50);
   // Create attachment
   const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
   return attachment;
