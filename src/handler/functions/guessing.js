1d485d60a3d6d2147e2ae7c43ea2393d49ceedbf
Naiyar <imamnaiyar29@gmail.com>
2024-01-25 08:06:45 +0600

    mm

--- src/handler/functions/guessing.js
+++ src/handler/functions/guessing.js
@@ -6,7 +6,6 @@ const {
 } = require('discord.js');
 const Canvas = require('@napi-rs/canvas');
 const { request } = require('undici');
-const config = require('@root/config')
 const questions = require('./questions');
  const gameData = new Map();
 module.exports = async (interaction, total) => {
@@ -184,7 +183,7 @@ const applyText = (canvas, text) => {
 
   do {
     // Assign the font to the context and decrement it so it can be measured again
-    context.font = `${fontSize -= 10}px sans-serif`;
+    context.font = `bold ${fontSize -= 10}px sans-serif`;
     // Compare pixel width of the text to the canvas minus the approximate avatar size
   } while (context.measureText(text).width > canvas.width - 300);
 
@@ -199,7 +198,7 @@ async function getWinnerImg(client, member, points) {
   // Load avatar image
   const avtr = await request(member.displayAvatarURL({ format: 'jpg', size: 512 }));
   const avatar = await Canvas.loadImage(await avtr.body.arrayBuffer());
-  const botAvtr = await request(config.BOT_ICON);
+  const botAvtr = await request('https://skyhelper.xyz/assets/img/boticon.png');
   const botAvatar = await Canvas.loadImage(await botAvtr.body.arrayBuffer());
   const winnerFrame = await Canvas.loadImage('https://media.discordapp.net/attachments/867638574571323424/1199827121879662702/winner-frame.png');
 
@@ -288,7 +287,7 @@ context.drawImage(botAvatar, botX, botY, 50, 50);
  context.restore();
  context.font = 'bold 22px sans-serif';
 context.fillStyle = '#860f0f';
-context.fillText('SkyHelper', canvas.width - 135, canvas.height - 40);
+context.fillText('SkyHelper', canvas.width - 140, canvas.height - 37);
   // Create attachment
   const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
   return attachment;
