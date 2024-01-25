12c31cd3d60b90081c667982ee934c6a747bcd39
Naiyar <imamnaiyar29@gmail.com>
2024-01-25 07:22:26 +0600

    mm

--- src/handler/functions/guessing.js
+++ src/handler/functions/guessing.js
@@ -280,7 +280,7 @@ context.beginPath();
 context.arc(canvas.width - 75, canvas.height - 75, 25, 0, Math.PI * 2, true);
 context.closePath();
 context.clip();
-context.drawImage(botAvatar, canvas.width - 100, canvas.height - 100, 50, 50);
+context.drawImage(botAvatar, canvas.width - 200, canvas.height - 50, 50, 50);
 context.restore();
   // Create attachment
   const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'profile-image.png' });
