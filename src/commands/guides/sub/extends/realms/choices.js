24f4b6ad0bc8b3466b58351402a675664037d212
Naiyar <imamnaiyar29@gmail.com>
2024-02-09 21:57:54 +0530

    added choices for realms

---
+++ src/commands/guides/sub/extends/realms/choices.js
@@ -0,0 +1,37 @@
+const firstChoices = [
+  {
+    label: 'Isle of Dawn',
+    value: 'dawn',
+    emoji: '',
+  },
+  {
+    label: 'Daylight Prairie',
+    value: 'prairie',
+    emoji: '',
+  },
+  {
+    label: 'Hidden Forest',
+    value: 'forest',
+    emoji: '',
+  },
+  {
+    label: 'Valley of Triumph',
+    value: 'valley',
+    emoji: '',
+  },
+  {
+    label: 'Golden Wasteland',
+    value: 'wasteland',
+    emoji: '',
+  },
+  {
+    label: 'Vault of Knowledge',
+    value: 'vault',
+    emoji: '',
+  },
+  {
+    label: 'Eye of Eden',
+    value: 'eden',
+    emoji: '',
+  },
+  ];
--- src/commands/guides/sub/shared/helpers.js
+++ src/commands/guides/sub/shared/helpers.js
@@ -1,25 +1,40 @@
 const { StringSelectMenuBuilder, ActionRowBuilder } = require("discord.js");
+
 module.exports =  {
+  /**
+   * Returns StringSelect Menu ActionRow for guides
+   * @param {string} customId - The custom Id for the select menu
+   * @param {object} choices - The options to include in swlect menu
+   * @param {string} placeholder - Placeholder for the select
+   * @param {Boolean} back - Whether to include back option or not
+   */
   rowBuilder: (customId, choices, placeholder, back) => {
     if (back) choices.push({
       label: "Back",
       value: "back",
       emoji: "⬅️",
-    })
+    });
     return new ActionRowBuilder().addComponents(new StringSelectMenuBuilder()
     .setCustomId(customId)
     .setPlaceholder(placeholder)
     .addOptions(choices));
   },
-
+  
+  /**
+   * Responds with the chosen guide
+   * @param {import('discord.js').Interaction} int - The SelectMenu Interaction
+   * @param {object} answers - The guides to search from
+   * @param {string} value - the value to search for
+   * @param {Boolean} ephemeral - Whether reply should be ephemeral
+   */
   respond: async (int, answers, value, ephemeral) => {
-    await int.deferReply({ ephemeral: ephemeral })  
-    const response = answers.getResponse(value)
+    await int.deferReply({ ephemeral: ephemeral });
+    const response = answers.getResponse(value);
     if (!response) {
-      return int.followUp("Guide is still under development. Thank you for your patience")
+      return int.followUp("Guide is still under development. Thank you for your patience");
     }
-    await int.followUp(response)
+    await int.followUp(response);
     return;
   }
-}
+};
   
