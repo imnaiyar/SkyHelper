6951f61221e8fd7dcf9b213d3d246f2b9e952baf
NyR <imamnaiyar29@gmail.com>
2024-02-16 15:14:01 +0000

    mm

--- src/commands/ts.js
+++ src/commands/ts.js
@@ -1,4 +1,4 @@
-
+const moment = require('moment-timezone')
 
 /**
  * @type {import('@src/structures').SlashCommands}
@@ -12,8 +12,21 @@
      longDesc: 'To be decided'
    },
    
-   async execute() {
-     const [ name, location, tree, total ] = client.ts;
-     const nm = name ? name : 'Unknown';
+   async execute(interaction client) {
+     const [ lastOccurence, name, location, tree, total ] = client.ts;
+     const now = moment().tz(client.timezone)
+     const lastDate = moment.tz(lastOccurrence, 'DD-MM-YYYY', client.timezone).startOf('day')
+     const nextDate = lastDate.clone()
+     while (now.isAfter(lastDate)) {
+       nextDate.add(14, 'days')
+     }
+     const prev = nextDate.clone().subtract(14, 'day')
+     const stay = prev.clone().add(3, 'day')
+     let embed;
+     if (now.isBetween(prev, stay)) {
+       if (name) {
+         
+       }
+     }
    }
  }
