9528aee9f3968a3ad963a3553b83a8e4052c360a
imnaiyar <imamnaiyar29@gmail.com>
2024-03-27 21:55:38 +0000

    some fixes

--- src/commands/shards/autoShard.js
+++ src/commands/shards/autoShard.js
@@ -1,111 +1,3 @@
-<<<<<<< HEAD
-const { ApplicationCommandOptionType } = require('discord.js');
-const moment = require('moment-timezone');
-const { buildShardEmbed, deleteSchema } = require('@functions');
-const { autoShard } = require('@schemas/autoShard');
-const desc = require('@src/cmdDesc');
-module.exports = {
-  data: {
-    name: 'shards-live',
-    description: 'auto updating message with live shards details',
-    options: [
-      {
-        name: 'start',
-        description: 'configure auto shard',
-        type: ApplicationCommandOptionType.Subcommand,
-        options: [
-          {
-            name: 'channel',
-            description: 'channel where shard details should be updated',
-            type: ApplicationCommandOptionType.Channel,
-            required: true,
-          },
-        ],
-      },
-      {
-        name: 'stop',
-        description: 'stop auto shard',
-        type: ApplicationCommandOptionType.Subcommand,
-      },
-    ],
-    dm_permission: false,
-    longDesc: desc.autoShard,
-    userPermissions: ['ManageGuild'],
-  },
-  async execute(interaction, client) {
-    await interaction.deferReply({ ephemeral: true });
-    if (!interaction.guild) {
-      return interaction.followUp('This command can only be used in a server');
-    }
-    const sub = interaction.options.getSubcommand();
-    const config = await autoShard(interaction.guild);
-    if (sub === 'start') {
-      if (config.channelId && config.messageId) {
-        const ch = await client.channels.fetch(config.channelId).catch((err) => {});
-        const ms = await ch?.messages.fetch(config.messageId).catch((err) => {});
-        if (ms && ch) {
-          return interaction.followUp({
-            content: `Live Shard is already configured in <#${config.channelId}> for this message ${ms.url}.`,
-          });
-        }
-      }
-      const channel = interaction.options.getChannel('channel');
-      if (!channel.isTextBased() || channel.isVoiceBased()) {
-        return interaction.followUp({ content: `${channel} is not a text channel or is a voice channel. Please provide a valid text channel`})
-      }
-      const requiredPerms = ['SendMessages', 'ViewChannel'];
-      const missingPerms = [];
-
-      for (const perm of requiredPerms) {
-        if (!interaction.guild.members.me.permissionsIn(channel).has(perm)) {
-          missingPerms.push(perm);
-        }
-      }
-
-      if (missingPerms.length > 0) {
-        return interaction.followUp({
-          content: `I do not have the required permissions (${missingPerms
-            .map((prm) => `\`${prm}\``)
-            .join(', ')}) to perform this action in <#${channel.id}>`,
-          ephemeral: true,
-        });
-      }
-
-      const currentDate = moment().tz(interaction.client.timezone);
-      const updatedAt = Math.floor(currentDate.valueOf() / 1000);
-      const { result } = await buildShardEmbed(currentDate, 'Live Shard');
-      const msg = await channel.send({
-        content: `Last Updated: <t:${updatedAt}:R>`,
-        embeds: [result],
-      });
-      config.channelId = channel.id;
-      config.messageId = msg.id;
-      await config.save();
-      interaction.followUp({
-        content: `Live Shard configured for <#${channel.id}>. This message ${msg.url} will be updated every 5 minutes with live Shards details.`,
-      });
-    } else if (sub === 'stop') {
-      if (!config.channelId || !config.messageId) {
-        return interaction.followUp({
-          content: 'Live Shard is already disabled for this server',
-        });
-      }
-      const ch = client.channels.cache.get(config.channelId);
-      ch.messages.fetch(config.messageId).then((m) => {
-        if (m) {
-          m.delete();
-        }
-      });
-
-      await deleteSchema('autoShard', interaction.guild.id);
-
-      interaction.followUp({
-        content: 'Live Shard is disabled',
-      });
-    }
-  },
-};
-=======
 const { ApplicationCommandOptionType, WebhookClient, MessageFlags, ChannelType } = require("discord.js");
 const moment = require("moment-timezone");
 const { autoShard } = require("@schemas/autoShard");
@@ -177,7 +69,7 @@ module.exports = {
       }
       const name = interaction.options.getString("name");
 
-      const wb = await client.createWebhook(channel, "For live Shards Update", name, client.displayAvatarURL());
+      const wb = await client.createWebhook(channel, "For live Shards Update", name, client.user.displayAvatarURL());
       const currentDate = moment().tz(interaction.client.timezone);
       const updatedAt = Math.floor(currentDate.valueOf() / 1000);
       const { result } = await buildShardEmbed(currentDate, "Live Shard");
@@ -219,4 +111,3 @@ module.exports = {
     }
   },
 };
->>>>>>> quiz-branch
--- src/functions/timesUpdate.js
+++ src/functions/timesUpdate.js
@@ -20,25 +20,6 @@ module.exports = async (client) => {
     deleteSchema("autoTimes", id);
   };
   data.forEach(async (guild) => {
-    if (!guild?.webhook?.id && guild.channelId && guild.messageId ) {
-      const ch = client.channels.cache.get(guild.channelId);
-      const orgMsg = await ch.messages.fetch(guild.messageId).catch(() => {});
-      if (orgMsg) 
-      {
-       orgMsg.edit({
-        embeds: [],
-        content: `🔔 Attention:
-
-Hey everyone, bot developer here, I Initially designed the bot with smaller communities in mind, I never expected it to join more than 20 servers let alone this much. However, with expansion comes the need for careful resource management and optimization to ensure optimal performance for all users.
-
-To address these concerns, I am implementing significant changes to enhance efficiency and performance. Specifically, the bot will transition from using channel IDs to utilizing webhooks for Live Updates (Auto Updates). While the previous method was suitable for smaller scales, it could potentially lead to API spam and suboptimal performance at larger scales.
-
-Please note that as a result of this transition, all existing Auto Updates will cease to function until they are reconfigured using </shards-live start:1187023189402996837> </sky-times-live start:1187498083110637662> (Make sure to grant the bot \`Manage Webhooks\` permission beforehand). I apologize for any inconvenience this may cause. There are other major changes made, you can check them all by running </util changelog:1161676078385995786>`
-      }).catch((err) => client.logger.error(`Guild: ${guild.name ? guild.name : 'Unknown'} (${guild._id}  )`, err));
-      dltSChm(guild._id);
-      }
-      return;
-      }
     if (!guild?.webhook?.id) return;
     const webhook = await client.fetchWebhook(guild.webhook.id, guild.webhook.token).catch(() => {});
     if (!webhook) {
