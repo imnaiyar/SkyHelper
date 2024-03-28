401de01263ab8cb34d1d9d7e2c592aa6dd8370a8
imnaiyar <imamnaiyar29@gmail.com>
2024-03-28 23:55:20 +0000

    hh

--- src/commands/shards/autoShard.js
+++ src/commands/shards/autoShard.js
@@ -78,7 +78,9 @@ module.exports = {
           ],
         });
       }
-
+      if (!channel.permissionsFor(interaction.guild.members.me).has('ManageWebhooks')) {
+          return await interaction.editReply(`I do not have \`Manage Webhooks\` permission in ${ch}. Please make sure that there is no channel level permission overwrides and if there is, please grant me the necessary permissions in the said channel before running the command again.`);
+        }
       const wb = await client.createWebhook(channel, "For live Shards Update");
       const currentDate = moment().tz(interaction.client.timezone);
       const updatedAt = Math.floor(currentDate.valueOf() / 1000);
