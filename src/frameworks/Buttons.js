62c6e597f143e7cc2060f227c9ef2ef921954260
imnaiyar <Imamnaiyar29@gmail.com>
2024-03-31 05:17:25 +0600

    some changes

---
+++ src/frameworks/Buttons.js
@@ -0,0 +1,13 @@
+const { ButtonInteraction } = require('discord.js');
+
+module.exports = {
+  name: '',
+  
+  /**
+   * Executes a button
+   * @param {ButtonInteraction} interaction
+   * @param {import('./SkyHelper.js')} client 
+   */
+  async execute(interaction, client) {
+  }
+}
