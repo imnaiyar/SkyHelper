58741b16dcc8efe96a15bca9c47799d1ca999c0f
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-06-16 21:57:06 +0530

    added unit tests

---
+++ jest.config.js
@@ -0,0 +1,31 @@
+/** @type { import('jest').Config } */
+const config = {
+    testEnvironment: 'node',
+    testTimeout: 30_000,
+    collectCoverage: true,
+    clearMocks: true,
+    coverageDirectory: 'coverage',
+    coverageProvider: 'v8',
+    roots: ['.'],
+
+    // ESM support:
+    extensionsToTreatAsEsm: ['.ts'],
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
+    transform: {
+        '^.+\\.m?[tj]sx?$': [
+            'ts-jest',
+            {
+                useESM: true,
+            },
+        ],
+    },
+
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        '/dist/',
+        '/docs/',
+    ],
+};
+export default config;
