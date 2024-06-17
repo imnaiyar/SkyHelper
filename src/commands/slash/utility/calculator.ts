2fc815ef1881416c2906c16fa2f26783a5ec0cdf
imnaiyar <imamnaiyar29@gmail.com>
2024-06-17 22:04:09 +0530

    mm

--- src/commands/slash/utility/calculator.ts
+++ src/commands/slash/utility/calculator.ts
@@ -4,64 +4,82 @@ import { SeasonCalculator, SeasonData as sn } from "#libs/index";
 import moment from "moment";
 import { useTranslations as x } from "#handlers/useTranslation";
 export default {
-  data: {
-    name: "seasonal-calculator",
-    name_localizations: x("commands.SEASONAL_CALCULATOR.name"),
-    description: "calculate seasonal currencies",
-    description_localizations: x("commands.SEASONAL_CALCULATOR.description"),
-    integration_types: [0, 1],
-    contexts: [0, 1, 2],
-    options: [
-      {
-        name: "candles",
-        name_localizations: x("commands.SEASONAL_CALCULATOR.options.CANDLES.name"),
-        description: "amount of candles you have?",
-        description_localizations: x("commands.SEASONAL_CALCULATOR.options.CANDLES.description"),
-        required: true,
-        type: ApplicationCommandOptionType.Integer,
-      },
-      {
-        name: "dailies",
-        name_localizations: x("commands.SEASONAL_CALCULATOR.options.DAILIES.name"),
-        description: "did you do your dailies today?",
-        description_localizations: x("commands.SEASONAL_CALCULATOR.options.DAILIES.description"),
-        required: true,
-        type: ApplicationCommandOptionType.Boolean,
-      },
-      {
-        name: "season-pass",
-        name_localizations: x("commands.SEASONAL_CALCULATOR.options.SEASON_PASS.name"),
-        description: "do you have the season pass?",
-        description_localizations: x("commands.SEASONAL_CALCULATOR.options.SEASON_PASS.description"),
-        required: false,
-        type: ApplicationCommandOptionType.Boolean,
-      },
-    ],
-  },
-  cooldown: 10,
-  category: "Utility",
-  async execute(interaction) {
-    const client = interaction.client;
-    // const type = interaction.options.getString("type");
-    await interaction.deferReply({ ephemeral: interaction.options.getBoolean("hide") ?? false });
-    const hasPass = interaction.options.getBoolean("season-pass")!;
-    const candles = interaction.options.getInteger("candles")!;
-    const dailies = interaction.options.getBoolean("dailies")!;
-    const now = moment().tz(client.timezone);
-    const end = moment.tz(sn.end, "MM-DD-YYYY", client.timezone);
-    // prettier-ignore
-    if (now.isAfter(end)) return void await interaction.followUp('No active season at the moment. Please run this command when a seson is active.');
-    const userData = await client.database.getUserData(interaction.user);
-    if (hasPass !== undefined) {
-      userData.hasPass = hasPass;
-      await userData.save();
+    data: {
+        name: "seasonal-calculator",
+        name_localizations: x("commands.SEASONAL_CALCULATOR.name"),
+        description: "calculate seasonal currencies",
+        description_localizations: x(
+            "commands.SEASONAL_CALCULATOR.description"
+        ),
+        integration_types: [0, 1],
+        contexts: [0, 1, 2],
+        options: [
+            {
+                name: "candles",
+                name_localizations: x(
+                    "commands.SEASONAL_CALCULATOR.options.CANDLES.name"
+                ),
+                description: "amount of candles you have?",
+                description_localizations: x(
+                    "commands.SEASONAL_CALCULATOR.options.CANDLES.description"
+                ),
+                required: true,
+                type: ApplicationCommandOptionType.Integer
+            },
+            {
+                name: "dailies",
+                name_localizations: x(
+                    "commands.SEASONAL_CALCULATOR.options.DAILIES.name"
+                ),
+                description: "did you do your dailies today?",
+                description_localizations: x(
+                    "commands.SEASONAL_CALCULATOR.options.DAILIES.description"
+                ),
+                required: true,
+                type: ApplicationCommandOptionType.Boolean
+            },
+            {
+                name: "season-pass",
+                name_localizations: x(
+                    "commands.SEASONAL_CALCULATOR.options.SEASON_PASS.name"
+                ),
+                description: "do you have the season pass?",
+                description_localizations: x(
+                    "commands.SEASONAL_CALCULATOR.options.SEASON_PASS.description"
+                ),
+                required: false,
+                type: ApplicationCommandOptionType.Boolean
+            }
+        ]
+    },
+    cooldown: 10,
+    category: "Utility",
+    async execute(interaction, t, client) {
+        // const type = interaction.options.getString("type");
+        await interaction.deferReply({
+            ephemeral: interaction.options.getBoolean("hide") ?? false
+        });
+        const hasPass = interaction.options.getBoolean("season-pass")!;
+        const candles = interaction.options.getInteger("candles")!;
+        const dailies = interaction.options.getBoolean("dailies")!;
+        const now = moment().tz(client.timezone);
+        const end = moment.tz(sn.end, "MM-DD-YYYY", client.timezone);
+        // prettier-ignore
+        if (now.isAfter(end)) return void await interaction.followUp(t("commands.SEASONAL_CALCULATOR.RESPONSES.NOT_ACTIVE"));
+        const userData = await client.database.getUserData(interaction.user);
+        if (hasPass !== undefined) {
+            userData.hasPass = hasPass;
+            await userData.save();
+        }
+        const calculator = new SeasonCalculator(
+            t,
+            interaction.inCachedGuild()
+                ? (interaction.member as GuildMember)
+                : interaction.user,
+            userData,
+            dailies,
+            candles,
+        );
+        await calculator.handleInt(interaction);
     }
-    const calculator = new SeasonCalculator(
-      interaction.inCachedGuild() ? (interaction.member as GuildMember) : interaction.user,
-      userData,
-      dailies,
-      candles,
-    );
-    await calculator.handleInt(interaction);
-  },
 } satisfies SlashCommand;
