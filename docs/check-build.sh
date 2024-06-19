8b525fc5085f0bb3e284a22beb72e043010b2121
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-06-20 01:23:28 +0530

    docs: incorrect return [skip ci]

--- docs/check-build.sh
+++ docs/check-build.sh
@@ -4,8 +4,8 @@ COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 
 if [[ $COMMIT_MESSAGE =~ "docs:" ]]; then
   echo "Deploy keyword found in commit message. Proceeding with deployment."
-  exit 0  # Exit with success status to allow deployment
+  exit 1  # Exit with success status to allow deployment
 else
   echo "Deploy keyword not found in commit message. Skipping deployment."
-  exit 1  # Exit with non-zero status to skip deployment
+  exit 0  # Exit with non-zero status to skip deployment
 fi
