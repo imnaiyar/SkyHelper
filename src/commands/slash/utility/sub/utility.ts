1cac2129cd4b44eb390735b20a468ded0150973e
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-06-19 23:56:37 +0530

    fix: version

--- src/commands/slash/utility/sub/utility.ts
+++ src/commands/slash/utility/sub/utility.ts
@@ -12,7 +12,8 @@ import {
   ModalSubmitInteraction,
 } from "discord.js";
 const suggWb = process.env.SUGGESTION ? new WebhookClient({ url: process.env.SUGGESTION }) : undefined;
-import { version } from "#root/package.json" assert { type: "json" };
+import pkg from "#root/package.json" assert { type: "json" };
+const version = pkg.version;
 export async function getSuggestion(interaction: ChatInputCommandInteraction, t: ReturnType<typeof getTranslator>) {
   const client = interaction.client;
   const attachment = interaction.options.getAttachment("attachment");
