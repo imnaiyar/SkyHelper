ad3a10af5a1dd409b7c4ffc0ab84cd0da7f4f7ff
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-06-22 20:22:04 +0530

    fix total visits

--- src/libs/classes/Spirits.ts
+++ src/libs/classes/Spirits.ts
@@ -80,7 +80,7 @@ export class Spirits {
                 ` **__${this.t("commands.GUIDES.RESPONSES.SPIRIT_SELECT_PLACEHOLDER", { SEASON: data.season })}__`,
             })}`
           : data.ts.returned
-            ? `${this.t("commands.SPIRITS.RESPONSES.EMBED.FIELDS.SUMMARY_DESC_RETURNED")}\n${data.ts.dates
+            ? `${this.t("commands.SPIRITS.RESPONSES.EMBED.FIELDS.SUMMARY_DESC_RETURNED", VISITS: data.ts.dates.length )}\n${data.ts.dates
                 .map((date) => {
                   let index;
                   const formatDate = date
