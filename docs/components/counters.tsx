10cfb4e4a26e917c8a85b221e21ef2dff7bd644a
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-06-28 20:17:13 +0530

    docs: buttons

--- docs/components/counters.tsx
+++ docs/components/counters.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
-import styles from "./counters.module.css";
+import { useState, useEffect } from "react";
 import bttn from "./button.module.css";
-
+import { useTheme } from "nextra-theme-docs";
 function MyButton() {
   const [count, setCount] = useState(0);
 
@@ -9,8 +8,14 @@ function MyButton() {
     setCount(count + 1);
   }
 
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    document.documentElement.style.setProperty("--button_outline_color", theme === "dark" ? "#983636" : "#000000");
+  }, [theme]);
+
   return (
-    <div>
+    <div className={bttn.container}>
       <button onClick={handleClick} className={bttn.button}>
         <span className={bttn.button_top}>Clicked {count} times</span>
       </button>
