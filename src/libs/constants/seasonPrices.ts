9dd8e80e92c9582f5d92242cd6928cc9bb425e86
imnaiyar <imamnaiyar29@gmail.com>
2024-07-15 14:37:36 +0600

    chore: spirits data for SODuet updated

--- src/libs/constants/seasonPrices.ts
+++ src/libs/constants/seasonPrices.ts
@@ -4,44 +4,51 @@ import type { SeasonPrice } from "#libs/types";
  * Spirit prices of the current season (For calculator)
  */
 export const SeasonPrices: { [key: string]: SeasonPrice } = {
-  "Nesting Nook": {
-    icon: "<:NestingNook:1238574218007019641>",
+  "The Cellist's Beginnings": {
+    icon: "<:CellistB:1262326135216406559>",
     cosmetics: [
-      { item: "Shelf", icon: "<:NookShelf:1238570642773774386>", price: 16 },
-      { item: "Blessings Node", icon: "<:BlessingNode:1238570665267691602>", price: 20 },
-      { item: "Spice Rack", icon: "<:NookSpiceRack:1238570686126231603>", price: 26 },
-      { item: "Blessings Node", canSkip: true, icon: "<:BlessingNode:1238570665267691602>", price: 30 },
-      { item: "Heart", icon: "<:NestingHeart:1238570703079342101>", price: 3, pass: true },
+      { item: "Hair", icon: "<:CellitBHair:1262322324963852308>", price: 20 },
+      { item: "Blessings Node", icon: "<:BlessingNode:1238570665267691602>", price: 26 },
+      { item: "Outfit", icon: "<:CellitBOutfit:1262322298145341581>", price: 32 },
+      { item: "Heart", icon: "<:DuetHeart:1262318647376871424>", price: 3, pass: true },
     ],
   },
-  "Nesting Atrium": {
-    icon: "<:NestingAtrium:1238574235778285679>",
+  "The Pianist's Beginnings": {
+    icon: "<:PianistB:1262326115024769107>",
     cosmetics: [
-      { item: "Floor Light", icon: "<:AtriumFloorLight:1238571195515801683>", price: 16 },
-      { item: "Blessings Node", icon: "<:BlessingNode:1238570665267691602>", price: 20 },
-      { item: "Nesting Atrium Hair", icon: "<:AtriumHair:1238571213559828480>", price: 24 },
-      { item: "Blessings Node", canSkip: true, icon: "<:BlessingNode:1238570665267691602>", price: 28 },
-      { item: "Heart", icon: "<:NestingHeart:1238570703079342101>", price: 3, pass: true },
+      { item: "Blessing Node", icon: "<:BlessingNode:1238570665267691602>", price: 14 },
+      { item: "Hair", icon: "<:PianistBHair:1262322985134461029>", price: 20 },
+      { item: "Blessings Node", icon: "<:BlessingNode:1238570665267691602>", price: 24 },
+      { item: "Small Blue Rug", icon: "<:SmallBlueRug:1262322964062277666>", price: 30},
+      { item: "Heart", icon: "<:DuetHeart:1262318647376871424>", price: 3, pass: true },
     ],
   },
-  "Nesting Loft": {
-    icon: "<:NestingLoft:1238574252534399078>",
+  "The Musicians' Legacy": {
+    icon: "<:MusicianL:1262326097341714514>",
     cosmetics: [
-      { item: "Blessings Node", icon: "<:BlessingNode:1238570665267691602>", price: 12 },
-      { item: "Chair", icon: "<:LoftChair:1238571566514831440>", price: 20 },
-      { item: "Blessing Nodes", icon: "<:BlessingNode:1238570665267691602>", price: 28 },
-      { item: "Paintings", icon: "<:LoftPaintings:1238571584118063104>", price: 36 },
-      { item: "Heart", icon: "<:NestingHeart:1238570703079342101>", price: 3, pass: true },
+      { item: "Music Sheet #40", icon: "<:MusicIcon:1262323496852131882>", price: 14 },
+      { item: "Blessing Node", icon: "<:BlessingNode:1238570665267691602>", price: 24 },
+      { item: "Instrument", icon: "<:MusiciansLInstrument:1262323472760049724>", price: 34 },
+      { item: "Heart", icon: "<:DuetHeart:1262318647376871424>", price: 3, pass: true },
     ],
   },
-  "Nesting Solarium": {
-    icon: "<:NestingSolarium:1238574270163058781> ",
+  "The Cellist's Flourishing": {
+    icon: "<:CellistF:1262326155508318380>",
     cosmetics: [
-      { item: "Blessings Node", icon: "<:BlessingNode:1238570665267691602>", price: 14 },
-      { item: "Hanging Planter", icon: "<:SolariumHangingPlanter:1238571757280169994>", price: 22 },
-      { item: "Blessings Node", icon: "<:BlessingNode:1238570665267691602>", price: 30 },
-      { item: "Table", icon: "<:SolariumTable:1238571738980155453>", price: 34 },
-      { item: "Heart", icon: "<:NestingHeart:1238570703079342101>", price: 3, pass: true },
+      { item: "Small Pink Rug", icon: "<:SmallPinkRug:1262323794715082824>", price: 16 },
+      { item: "Blessing Node", icon: "<:BlessingNode:1238570665267691602>", price: 18 },
+      { item: "Cape", icon: "<:CellistFCape:1262323774997659673>", price: 22 },
+      { item: "Blessings Node", icon: "<:BlessingNode:1238570665267691602>", price: 24 },
+      { item: "Heart", icon: "<:DuetHeart:1262318647376871424>", price: 3, pass: true },
+    ],
+  },
+  "The Pianist's Flourishing": {
+    icon: "<:PianistF:1262326075380334623>",
+    cosmetics: [
+      { item: "Blessing Nodes", icon: "<:BlessingNode:1238570665267691602>", price: 14 },
+      { item: "Shoes", icon: "<:PianistFShoes:1262324120532549692>", price: 22 },
+      { item: "Blessing Node", icon: "<:BlessingNode:1238570665267691602>", price: 26 },
+      { item: "Heart", icon: "<:DuetHeart:1262318647376871424>", price: 3, pass: true },
     ],
   },
 } as const;
@@ -52,5 +59,5 @@ export const SeasonData = {
   start: "15-07-2024",
   end: "29-09-2024",
   duration: 77,
-  spiritsUpdated: false
+  spiritsUpdated: true
 } as const;
