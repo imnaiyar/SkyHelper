b4e5fe3ad4e3fdce01b1e1a45a665d7d53e7dbe5
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-08-07 18:14:19 +0530

    chore: uncomment stuff

--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,7 @@ Sentry.init({
   ],
   environment: process.env.NODE_ENV,
 
-  // Performance Monitoring
-  tracesSampleRate: 1.0, //  Capture 100% of the transactions
-
-  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  tracesSampleRate: 1.0, 
   profilesSampleRate: 1.0,
 });
 
