fccc72fbd524671f27f1e600a8d4864bf683b3b8
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-08-14 12:58:55 +0530

    fix: only import know filepaths

--- src/libs/constants/spirits-datas/base/index.ts
+++ src/libs/constants/spirits-datas/base/index.ts
@@ -7,7 +7,9 @@ import { fileURLToPath } from "url";
 // Not using `import.meta.dirname` here because it is returning undefined in jest leading to tests being failed
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const paths = readdirSync(__dirname).filter((file) => file.split(".")[0] !== "index");
+const paths = readdirSync(__dirname).filter(
+  (file) => file.split(".")[0] !== "index" && (file.endsWith(".js") || file.endsWith(".ts")),
+);
 
 for (const p of paths) {
   const { default: data } = (await import(path.join(__dirname, p))) as {
--- src/libs/constants/spirits-datas/seasonal/index.ts
+++ src/libs/constants/spirits-datas/seasonal/index.ts
@@ -8,7 +8,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 let toExport: Record<string, SpiritsData> = {};
-const paths = readdirSync(__dirname).filter((file) => file.split(".")[0] !== "index");
+const paths = readdirSync(__dirname).filter(
+  (file) => file.split(".")[0] !== "index" && (file.endsWith(".js") || file.endsWith(".ts")),
+);
 
 for (const p of paths) {
   const { default: data } = (await import(path.join(__dirname, p))) as {
