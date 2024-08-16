1d1fd7ea08a16685243eb600e2cd0bbb1909380b
imnaiyar <137700126+imnaiyar@users.noreply.github.com>
2024-08-16 13:14:25 -0500

    feat: talented builder

--- src/bot/libs/constants/spirits-datas/seasonal/11-flight.ts
+++ src/bot/libs/constants/spirits-datas/seasonal/11-flight.ts
@@ -97,6 +97,7 @@ const data: Record<string, SpiritsData> = {
   },
   talented_builder: {
     name: "Talented Builder",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/ac/Flight-Spirit-Talented-Builder.png",
     type: "Seasonal Spirit",
     realm: "Hidden Forest",
     season: "Flight",
@@ -114,7 +115,6 @@ const data: Record<string, SpiritsData> = {
       by: "Clement",
       image: "Talented_Builder_Location_Clement.png",
     },
-
     emote: {
       icon: "<:builder:1131589149464481812>",
       level: [
@@ -136,6 +136,76 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Outfit",
+        icon: "<:BuilderOutfit:1274063253521039401>",
+        type: "Outfit",
+        price: "70 <:RegularCandle:1207793250895794226>",
+        isSP: true,
+        images: [
+          {
+            description: "Outfit (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/45/Season_of_flight_pants_2_talented_builder_gs.PNG/revision/latest/scale-to-width-down/400?cb=20230626013814",
+          },
+          {
+            description: "Outfit (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/bd/Talented-Builder-Outfit-Back.png/revision/latest/scale-to-width-down/400?cb=20240225200623",
+          },
+        ],
+      },
+      {
+        name: "White ruff",
+        type: "Neck Accessory",
+        icon: "<:BuilderNecklace:1274063232105189497>",
+        price: "40 <:RegularCandle:1207793250895794226>",
+        spPrice: "16 <:FlightCandle:1274063266389430312>",
+        images: [
+          {
+            description: "The Ruff",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a0/Season_of_flight_neckpiece_talented_builder.png/revision/latest/scale-to-width-down/400?cb=20210904225558",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        type: "Hair",
+        icon: "<:BuilderHair:1274062847068082361>",
+        price: "45 <:RegularCandle:1207793250895794226>",
+        spPrice: "26 <:FlightCandle:1274063266389430312>",
+        images: [
+          {
+            description: "Hair (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/46/Season_of_flight_hair_1_talented_builder_gs.png/revision/latest/scale-to-width-down/400?cb=20240222065757",
+          },
+          {
+            description: "Hair (Side)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Talented-Builder-Hair-side.png/revision/latest/scale-to-width-down/400?cb=20240222072933",
+          },
+          {
+            description: "Hair (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/e/e8/Talented-Builder-Hair-back.png/revision/latest/scale-to-width-down/400?cb=20240222072937",
+          },
+        ],
+        notes: [
+          "Note that this item is considered a Hair, not Hair Accessory, and such it can not be worn over other hairstyles",
+        ],
+      },
+      {
+        name: "Music Sheet #25",
+        type: "Music Sheet",
+        icon: "<:MusicIcon:1262323496852131882>",
+        price: "15 <:RegularCandle:1207793250895794226>",
+        isSP: true,
+        images: [],
+      },
+    ],
   },
 };
 
