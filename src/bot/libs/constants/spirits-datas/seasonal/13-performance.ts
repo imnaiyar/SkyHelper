ac5301b53aedf4bb2967e66e397500717019f320
imnaiyar <137700126+imnaiyar@users.noreply.github.com>
2024-08-18 05:56:05 -0500

    feat: stagehand and storyteller

--- src/bot/libs/constants/spirits-datas/seasonal/13-performance.ts
+++ src/bot/libs/constants/spirits-datas/seasonal/13-performance.ts
@@ -80,6 +80,7 @@ const data: Record<string, SpiritsData> = {
   stagehand: {
     name: "Frantic Stagehand",
     type: "Seasonal Spirit",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c0/Performance-Spirit-Frantic-Stagehand.png",
     realm: "Valley of Triumph",
     season: "Performance",
     ts: {
@@ -96,7 +97,6 @@ const data: Record<string, SpiritsData> = {
       by: "SkyZed",
       image: "Frantic_Stagehand_Location_SkyZed.jpg",
     },
-
     action: {
       icon: "<:stagehand:1131586475226562670>",
       level: [
@@ -110,9 +110,67 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Outfit",
+        icon: "<:StagehandOutfit:1274680011894554634>",
+        type: "Outfit",
+        price: "70 <:RegularCandle:1207793250895794226>",
+        isSP: true,
+        images: [
+          {
+            description: "The Outfit (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/0/02/SoPerformance_-_Frantic_Stagehand-outfit.png/revision/latest/scale-to-width-down/400?cb=20230626014208",
+          },
+          {
+            description: "The Outfit (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/5/5b/Frantic-Stagehand-Outfit-Back.png/revision/latest/scale-to-width-down/400?cb=20240225195716",
+          },
+        ],
+      },
+      {
+        name: "Mask",
+        type: "Mask",
+        icon: "<:StagehandMask:1274679946689646723>",
+        price: "34 <:RegularCandle:1207793250895794226>",
+        spPrice: "30 <:PerformanceCandle:1274667488323571722>",
+        images: [
+          {
+            description: "The Mask",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/8/81/SoPerformance_-_Frantic_Stagehand-mask.png/revision/latest/scale-to-width-down/400?cb=20220312223201",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        icon: "<:StagehandHair:1274679897373278259>",
+        price: "48 <:RegularCandle:1207793250895794226>",
+        isSP: true,
+        images: [
+          {
+            description: "The Hair",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/e/e3/SoPerformance_-_Frantic_Stagehand-hairstyle.png/revision/latest/scale-to-width-down/400?cb=20240305020245",
+          },
+        ],
+      },
+      {
+        name: "Music Sheet #29",
+        icon: "<:MusicIcon:1262323496852131882>",
+        type: "Music Sheet",
+        price: "15 <:RegularCandle:1207793250895794226>",
+        spPrice: "22 <:PerformanceCandle:1274667488323571722>",
+        images: [],
+      },
+    ],
   },
+
   storyteller: {
     name: "Forgetful Storyteller",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/1e/Performance-Spirit-Forgetful-Storyteller.png",
     type: "Seasonal Spirit",
     realm: "Valley of Triumph",
     season: "Performance",
@@ -130,7 +188,6 @@ const data: Record<string, SpiritsData> = {
       by: "SkyZed",
       image: "Storyteller_Location_SkyZed.jpg",
     },
-
     emote: {
       icon: "<:storyteller:1131586451885260831>",
       level: [
@@ -152,6 +209,72 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Outfit",
+        icon: "<:StorytellerOutfit:1274667700580520008>",
+        type: "Outfit",
+        isSP: true,
+        images: [],
+      },
+      {
+        name: "Mask",
+        icon: "<:StorytellerMask:1274667589091594280>",
+        type: "Mask",
+        isSP: true,
+        images: [
+          {
+            description: "The Mask",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/24/SoPerformance_-_Forgetful_Storyteller-mask.png/revision/latest/scale-to-width-down/400?cb=20220306172212",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        type: "Hair",
+        icon: "<:StorytellerHair:1274667514399555604>",
+        spPrice: "16 <:PerformanceCandle:1274667488323571722>",
+        notes: [
+          " Note that this item is considered a Hair, not a Hair Accessory, and as such it can not be worn over other Hairstyles",
+        ],
+        images: [
+          {
+            description: "The Hair",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/da/SoPerformance_-_Forgetful_Storyteller-hairstyle.png/revision/latest/scale-to-width-down/400?cb=20220306172214",
+          },
+        ],
+      },
+      {
+        name: "Cape",
+        icon: "<:StorytellerCape:1274667502043009066>",
+        type: "Cape",
+        spPrice: "34 <:PerformanceCandle:1274667488323571722>",
+        images: [
+          {
+            description: "The Cape (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/6c/SOPerformance_-Forgetful_Storyteller_-_Cape_front.png/revision/latest/scale-to-width-down/400?cb=20220311011314",
+          },
+          {
+            description: "The Cape (Interior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/8/8b/SOPerformance_-Forgetful_Storyteller_-_Cape_front_butterly.png/revision/latest/scale-to-width-down/400?cb=20220311011313",
+          },
+          {
+            description: "The Cape (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/db/SOPerformance_-Forgetful_Storyteller_-_Cape_back.png/revision/latest/scale-to-width-down/400?cb=20220311011313",
+          },
+          {
+            description: "The Cape (Exterior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/9/9a/SOPerformance_-Forgetful_Storyteller_-_Cape_back_butterly.png/revision/latest/scale-to-width-down/400?cb=20220311011313",
+          },
+        ],
+      },
+    ],
   },
 };
 
