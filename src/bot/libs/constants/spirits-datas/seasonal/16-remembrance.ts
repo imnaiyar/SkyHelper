589dc205cbd25ff0163f06b4c4637bc28f6ecc29
imnaiyar <137700126+imnaiyar@users.noreply.github.com>
2024-08-18 11:55:46 -0500

    feat: remembrance spirits

--- src/bot/libs/constants/spirits-datas/seasonal/16-remembrance.ts
+++ src/bot/libs/constants/spirits-datas/seasonal/16-remembrance.ts
@@ -29,6 +29,7 @@ const data: Record<string, SpiritsData> = {
   },
   pleading_child: {
     name: "Pleading Child",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/d8/Remembrance-Spirit-Wounded-Warrior.png",
     type: "Seasonal Spirit",
     realm: "Vault of Knowledge",
     season: "Remembrance",
@@ -46,7 +47,6 @@ const data: Record<string, SpiritsData> = {
       by: "Mimi and Sam",
       image: "Remembrance_Spirits_Location_Mimi.png",
     },
-
     emote: {
       icon: "<:pleading:1131584152332935331>",
       level: [
@@ -68,10 +68,73 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Outfit",
+        icon: "<:WoundedWarriorOutfit:1274771681864454227>",
+        type: "Outfit",
+        spPrice: "30 <:RemembranceCandle:1274767706305859677>",
+        images: [
+          {
+            description: "The Outfit (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/41/SoRemembrance-Wounded-Warrior-Outfit.png/revision/latest/scale-to-width-down/400?cb=20230626022039",
+          },
+          {
+            description: "The Outfit (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/71/SoRemembrance-Wounded-Warrior-Outfit-Back.png/revision/latest/scale-to-width-down/400?cb=20240225152922",
+          },
+        ],
+      },
+      {
+        name: "Mask",
+        icon: "<:WoundedWarriorMask:1274771668551602268>",
+        type: "Mask",
+        isSP: true,
+        images: [
+          {
+            description: "The Mask",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/d4/SoRemembrance-Wounded-Warrior-Mask.png/revision/latest/scale-to-width-down/400?cb=20221218020600",
+          },
+        ],
+      },
+      {
+        name: "Cape",
+        icon: "<:WoundedWarriorCape:1274771654135779428>",
+        type: "Cape",
+        isSP: true,
+        images: [
+          {
+            description: "The Cape (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/e/e7/SoRemembrance-Wounded-Warrior-Cape-Front.png/revision/latest/scale-to-width-down/400?cb=20221218015720",
+          },
+          {
+            description: "The Cape (Interior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/67/SoRemembrance-Wounded-Warrior-Cape-Front-Butterfly.png/revision/latest/scale-to-width-down/400?cb=20221218015754",
+          },
+          {
+            description: "The Cape (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/b3/SoRemembrance-Wounded-Warrior-Cape-Back.png/revision/latest/scale-to-width-down/400?cb=20221218015804",
+          },
+          {
+            description: "The Cape (Exterior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/0/08/SoRemembrance-Wounded-Warrior-Cape-Back-Butterfly.png/revision/latest/scale-to-width-down/400?cb=20221218015811",
+          },
+        ],
+      },
+    ],
   },
+
   veteran: {
     name: "Bereft Veteran",
     type: "Seasonal Spirit",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/8/86/Remembrance-Spirit-Bereft-Veteran.png",
     realm: "Vault of Knowledge",
     season: "Remembrance",
     ts: {
@@ -88,7 +151,6 @@ const data: Record<string, SpiritsData> = {
       by: "Mimi and Sam",
       image: "Remembrance_Spirits_Location_Mimi.png",
     },
-
     emote: {
       icon: "<:Bereft:1131584129054560396>",
       level: [
@@ -110,9 +172,66 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Mask",
+        icon: "<:VeteranMask:1274769664085594245>",
+        type: "Mask",
+        spPrice: "6 <:RemembranceCandle:1274767706305859677>",
+        images: [
+          {
+            description: "The Mask",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/8/8d/SoRemembrance-Bereft-Veteran-Mask.png/revision/latest/scale-to-width-down/400?cb=20221217213207",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        icon: "<:VeteranHair:1274769648637968445>",
+        type: "Hair",
+        isSP: true,
+        images: [
+          {
+            description: "The Hair",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/6d/SoRemembrance-Bereft-Veteran-Hair.png/revision/latest/scale-to-width-down/400?cb=20221209024340",
+          },
+        ],
+      },
+      {
+        name: "Cape",
+        icon: "<:VeteranCape:1274769636428353616>",
+        type: "Cape",
+        spPrice: "34 <:RemembranceCandle:1274767706305859677>",
+        images: [
+          {
+            description: "The Cape (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/3d/SoRemembrance-Bereft-Veteran-Cape-front.png/revision/latest/scale-to-width-down/400?cb=20221218012602",
+          },
+          {
+            description: "The Cape (Interior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/3f/SoRemembrance-Bereft-Veteran-Cape-front-butterfly.png/revision/latest/scale-to-width-down/400?cb=20221218012602",
+          },
+          {
+            description: "The Cape (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/b7/SoRemembrance-Bereft-Veteran-Cape-back.png/revision/latest/scale-to-width-down/400?cb=20221218012602",
+          },
+          {
+            description: "The Cape (Exterior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/77/SoRemembrance-Bereft-Veteran-Cape-back-butterfly.png/revision/latest/scale-to-width-down/400?cb=20221218012600",
+          },
+        ],
+      },
+    ],
   },
   tea_brewer: {
     name: "Tiptoeing Tea-Brewer",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/e/ea/Remembrance-Spirit-Tiptoeing-Tea-Brewer.png",
     type: "Seasonal Spirit",
     realm: "Vault of Knowledge",
     season: "Remembrance",
@@ -130,7 +249,6 @@ const data: Record<string, SpiritsData> = {
       by: "Mimi and Sam",
       image: "Remembrance_Spirits_Location_Mimi.png",
     },
-
     emote: {
       icon: "<:tiptoe:1131584176882192455>",
       level: [
@@ -152,6 +270,67 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Outfit",
+        icon: "<:TeaBrewererOutfit:1274767756826247229>",
+        type: "Outfit",
+        isSP: true,
+        images: [
+          {
+            description: "The Outfit (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2a/SoRemembrance-Tiptoeing-Tea-Brewer-Outfit.png/revision/latest/scale-to-width-down/400?cb=20230627013140",
+          },
+          {
+            description: "The Outfit (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a8/SoRemembrance-Tiptoeing-Tea-Brewer-Outfit-Back.png/revision/latest/scale-to-width-down/400?cb=20240225152638",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        type: "Hair",
+        icon: "<:TeaBrewererHair:1274767734210564209>",
+        isSP: true,
+        images: [
+          {
+            description: "The Hair",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/4c/SoRemembrance-Tiptoeing-Tea-Brewer-Hair.png/revision/latest/scale-to-width-down/400?cb=20221218014039",
+          },
+        ],
+      },
+      {
+        name: "Cape",
+        type: "Cape",
+        icon: "<:TeaBrewererCape:1274767719664980048>",
+        spPrice: "38 <:RemembranceCandle:1274767706305859677>",
+        images: [
+          {
+            description: "The Cape (The Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/43/SoRemembrance-Tiptoeing-Tea-Brewer-Cape-Front.png/revision/latest/scale-to-width-down/400?cb=20221218014918",
+          },
+          {
+            description: "The Cape (Interior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/14/SoRemembrance-Tiptoeing-Tea-Brewer-Cape-Front-Butterfly.png/revision/latest/scale-to-width-down/400?cb=20221218014932",
+          },
+          {
+            description: "The Cape (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/b6/SoRemembrance-Tiptoeing-Tea-Brewer-Cape-Back.png/revision/latest/scale-to-width-down/400?cb=20221218014946",
+          },
+          {
+            description: "The Cape (Exterior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a6/SoRemembrance-Tiptoeing-Tea-Brewer-Cape-Back-Butterfly.png/revision/latest/scale-to-width-down/400?cb=20221218014958",
+          },
+        ],
+      },
+    ],
   },
 };
 
