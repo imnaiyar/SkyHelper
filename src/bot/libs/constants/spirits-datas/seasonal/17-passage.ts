7bc7f641d84e154163ae145a36c5cd6d8551c6c0
imnaiyar <137700126+imnaiyar@users.noreply.github.com>
2024-08-18 12:38:01 -0500

    feat: passage spirits

--- src/bot/libs/constants/spirits-datas/seasonal/17-passage.ts
+++ src/bot/libs/constants/spirits-datas/seasonal/17-passage.ts
@@ -3,6 +3,7 @@ import type { SpiritsData } from "#libs/types";
 const data: Record<string, SpiritsData> = {
   outcast: {
     name: "Oddball Outcast",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/11/Passage-Oddball-Outcast-Cutout.png",
     type: "Seasonal Spirit",
     realm: "Isle of Dawn",
     season: "Passage",
@@ -20,7 +21,6 @@ const data: Record<string, SpiritsData> = {
       by: "Clement",
       image: "Passage_Spirits_Location_Clement.jpg",
     },
-
     emote: {
       icon: "<:oddball:1131583444116308072>",
       level: [
@@ -42,9 +42,56 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Outfit",
+        icon: "<:OutcastOutfit:1274783081622012014>",
+        type: "Outfit",
+        spPrice: "32 <:PassageCandle:1274777223857049702>",
+        images: [
+          {
+            description: "The Outfir (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/ae/Passage-Oddball-Outcast-outfit-front.png/revision/latest/scale-to-width-down/400?cb=20230627013407",
+          },
+          {
+            description: "The Outfir (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2b/Oddball-Outcast-Outfit-Back.png/revision/latest/scale-to-width-down/400?cb=20240225200158",
+          },
+        ],
+      },
+      {
+        name: "Neck Accessory",
+        icon: "<:OutcastNeckAccessory:1274783067180896286>",
+        type: "Neck Accessory",
+        isSP: true,
+        images: [
+          {
+            description: "The Accessory",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/5/56/Passage-Oddball-Outcast-neck-accessory.png/revision/latest/scale-to-width-down/400?cb=20230323011211",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        icon: "<:OutcastHair:1274783051720953946>",
+        type: "Hair",
+        spPrice: "14 <:PassageCandle:1274777223857049702>",
+        images: [
+          {
+            description: "The Hair",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/0/08/Passage-Oddball-Outcast-Hairstyle.png/revision/latest/scale-to-width-down/400?cb=20230323011159",
+          },
+        ],
+      },
+    ],
   },
   overachiever: {
     name: "Overacting Overachiever",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a5/Passage-Overactive-Overachiever-Cutout.png",
     type: "Seasonal Spirit",
     realm: "Isle of Dawn",
     season: "Passage",
@@ -62,7 +109,6 @@ const data: Record<string, SpiritsData> = {
       by: "Clement",
       image: "Passage_Spirits_Location_Clement.jpg",
     },
-
     emote: {
       icon: "<:achiever:1131583465649876992>",
       level: [
@@ -84,9 +130,66 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Hair",
+        icon: "<:OverachieverHair:1274780794350731264>",
+        type: "Hair",
+        isSP: true,
+        images: [
+          {
+            description: "The Hair (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/e/ea/Passage-Overactive-Overachiever-Hair.png/revision/latest/scale-to-width-down/400?cb=20230323010218",
+          },
+          {
+            description: "The Hair (Side)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/29/Passage-Overactive-Overachiever-Hair-side.png/revision/latest/scale-to-width-down/400?cb=20230323010219",
+          },
+        ],
+      },
+      {
+        name: "Cape",
+        icon: "<:OverachieverCape:1274780778596663337>",
+        type: "Cape",
+        spPrice: "30 <:PassageCandle:1274777223857049702>",
+        images: [
+          {
+            description: "The Cape (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/ac/Passage-Overactive-Overachiever-Cape-front.png/revision/latest/scale-to-width-down/400?cb=20230322214510",
+          },
+          {
+            description: "The Cape (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/9/9a/Passage-Overactive-Overachiever-Cape-back.png/revision/latest/scale-to-width-down/400?cb=20230322214509",
+          },
+        ],
+      },
+      {
+        name: "Manta Ocarina",
+        icon: "<:OverachieverInstrument:1274780770145403062>",
+        type: "Instrument",
+        isSP: true,
+        images: [
+          {
+            description: "The Instrument",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2f/Passage-Overactive-Overachiever-Instrument.png/revision/latest/scale-to-width-down/400?cb=20230322214515",
+          },
+          {
+            description: "On player's back",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/8/89/Passage-Overactive-Overachiever-Instrument-on-back.png/revision/latest/scale-to-width-down/400?cb=20230322214518",
+          },
+        ],
+      },
+    ],
   },
   mope: {
     name: "Melancholy Mope",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/7a/Passage-Melancholy-Mope-Cutout.png",
     type: "Seasonal Spirit",
     realm: "Isle of Dawn",
     season: "Passage",
@@ -104,7 +207,6 @@ const data: Record<string, SpiritsData> = {
       by: "Clement",
       image: "Passage_Spirits_Location_Clement.jpg",
     },
-
     emote: {
       icon: "<:mope:1131583421529989211>",
       level: [
@@ -126,9 +228,66 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Outfit",
+        icon: "<:MopeOutfit:1274778986005925909>",
+        type: "Outfit",
+        spPrice: "28 <:PassageCandle:1274777223857049702>",
+        images: [
+          {
+            description: "The Outfit (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/8/8f/Passage-Melancholy-Mope-Outfit-front.png/revision/latest/scale-to-width-down/400?cb=20230627013258",
+          },
+          {
+            description: "The Outfit (Interior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c4/Passage-Melancholy-Mope-Outfit-front-butterfly.png/revision/latest/scale-to-width-down/400?cb=20230322214341",
+          },
+          {
+            description: "The Outfit (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a9/Passage-Melancholy-Mope-Outfit-back.png/revision/latest/scale-to-width-down/400?cb=20230322214338",
+          },
+          {
+            description: "The Outfit (Exterior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/d4/Passage-Melancholy-Mope-Outfit-back-butterfly.png/revision/latest/scale-to-width-down/400?cb=20230322214339",
+          },
+        ],
+      },
+      {
+        name: "Earrings",
+        icon: "<:MopeEarings:1274778965604569221>",
+        type: "Face Accessory",
+        spPrice: "6 <:PassageCandle:1274777223857049702>",
+        images: [
+          {
+            description: "The Accessory",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/de/Passage-Melancholy-Mope-Hair-accessory.png/revision/latest/scale-to-width-down/400?cb=20240726204345",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        icon: "<:MopeHair:1274778950077251607>",
+        type: "Hair",
+        isSP: true,
+        images: [
+          {
+            description: "The Hair",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/9/9f/Passage-Melancholy-Mope-Hair.png/revision/latest/scale-to-width-down/400?cb=20230323010554",
+          },
+        ],
+      },
+    ],
   },
   troublemaker: {
     name: "Tumbling Troublemaker",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/e/ee/Passage-Tumbling-Troublemaker-Cutout.png",
     type: "Seasonal Spirit",
     realm: "Isle of Dawn",
     season: "Passage",
@@ -146,7 +305,6 @@ const data: Record<string, SpiritsData> = {
       by: "Clement",
       image: "Passage_Spirits_Location_Clement.jpg",
     },
-
     emote: {
       icon: "<:tumbling:1131583486940164126>",
       level: [
@@ -168,6 +326,62 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Earings",
+        icon: "<:TroublemakerFaceAcessory:1274777276600291398>",
+        type: "Face Accessory",
+        isSP: true,
+        images: [
+          {
+            description: "The Accessory",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/27/Passage-Tumbling-Troublemaker-Hair-Accessory.png/revision/latest/scale-to-width-down/400?cb=20230323011033",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        icon: "<:TroublemakerHair:1274777254093652079>",
+        type: "Hair",
+        isSP: true,
+        images: [
+          {
+            description: "The Hair",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/24/Passage-Tumbling-Troublemaker-Hair.png/revision/latest/scale-to-width-down/400?cb=20230323011032",
+          },
+        ],
+      },
+      {
+        name: "Cape",
+        icon: "<:TroublemakerCape:1274777239967236200>",
+        type: "Cape",
+        isSP: true,
+        images: [
+          {
+            description: "The Cape (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a4/Passage-Tumbling-Troublemaker-Cape-front.png/revision/latest/scale-to-width-down/400?cb=20230325023621",
+          },
+          {
+            description: "The Cape (Interior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/3f/Passage-Tumbling-Troublemaker-Cape-front-butterfly.png/revision/latest/scale-to-width-down/400?cb=20230325023622",
+          },
+          {
+            description: "The Cape (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c0/Passage-Tumbling-Troublemaker-Cape-back.png/revision/latest/scale-to-width-down/400?cb=20230325023618",
+          },
+          {
+            description: "The Cape (Exterior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/ac/Passage-Tumbling-Troublemaker-Cape-back-butterfly.png/revision/latest/scale-to-width-down/400?cb=20230325023620",
+          },
+        ],
+      },
+    ],
   },
 };
 
