2ff45a52c30eaceb5f75f1277e9bc1992927cdc0
imnaiyar <137700126+imnaiyar@users.noreply.github.com>
2024-08-19 09:35:11 -0500

    feat: isle spirits

--- src/bot/libs/constants/spirits-datas/base/isle.ts
+++ src/bot/libs/constants/spirits-datas/base/isle.ts
@@ -4,6 +4,7 @@ const data: Record<string, SpiritsData> = {
   candlemaker: {
     name: "Pointing Candlemaker",
     type: "Regular Spirit",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/5/54/Isle-Spirit-Pointing-Candlemaker.png",
     realm: "Isle of Dawn",
     main: {
       description: "Pointing Candlemaker Infographics by Ed.7",
@@ -31,9 +32,43 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Outfit",
+        icon: "<:PointingCandlemakerOutfit:1275100068000698490>",
+        type: "Outfit",
+        price: "4 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "The Outfit",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/16/Legs07.png/revision/latest/scale-to-width-down/400?cb=20230626011050",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        icon: "<:PointingCandlemakerHair:1275100055921233920>",
+        type: "Hair",
+        price: "Free",
+        images: [
+          {
+            description: "The Hair (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/75/Hair18-Short_Cowlick.png/revision/latest/scale-to-width-down/400?cb=20240125085423",
+          },
+          {
+            description: "The Hair (Side)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/15/Pointing-Candlemaker-Hair-side.png/revision/latest/scale-to-width-down/400?cb=20240128041656",
+          },
+        ],
+      },
+    ],
   },
   stargazer: {
     name: "Ushering Stargazer",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a4/Isle-Spirit-Ushering-Stargazer.png",
     type: "Regular Spirit",
     realm: "Isle of Dawn",
     main: {
@@ -61,9 +96,38 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Outfit",
+        icon: "<:UsheringStargazerOutfit:1275099169698217985>",
+        price: "4 <:regularHeart:1207793247792013474>",
+        type: "Outfit",
+        images: [
+          {
+            description: "The outfit",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/cf/Legs06.png/revision/latest/scale-to-width-down/400?cb=20230626011203",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        icon: "<:UsheringStargazerHair:1275099156200951918>",
+        type: "Hair",
+        price: "Free",
+        images: [
+          {
+            description: "The Hair",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/f7/Hair06-Single_Ponytail.png/revision/latest/scale-to-width-down/400?cb=20240121085912",
+          },
+        ],
+      },
+    ],
   },
   voyager: {
     name: "Rejecting Voyager",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/9/9a/Isle-Spirit-Rejecting-Voyager.png",
     type: "Regular Spirit",
     realm: "Isle of Dawn",
     main: {
@@ -91,6 +155,34 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Head guard face accessory",
+        icon: "<:RejectingVoyagerFaceAccessory:1275097633840435272>",
+        type: "Face Accessory",
+        price: "3 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "The Accessory",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/0/0d/Mask02-Simple_horizontal.png/revision/latest/scale-to-width-down/400?cb=20210810231600",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        icon: "<:RejectingVoyagerHair:1275097617134653450>",
+        price: "1 <:regularHeart:1207793247792013474>",
+        type: "Hair",
+        images: [
+          {
+            description: "The Hair",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/e/e6/Hair34-One_Side_Short.png/revision/latest/scale-to-width-down/400?cb=20240205031609",
+          },
+        ],
+      },
+    ],
   },
 };
 
