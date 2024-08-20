809caa5322567b7880e9f9892abe8959aec10dff
imnaiyar <137700126+imnaiyar@users.noreply.github.com>
2024-08-19 22:13:44 -0500

    feat: forest spirits

--- src/bot/libs/constants/spirits-datas/base/forest.ts
+++ src/bot/libs/constants/spirits-datas/base/forest.ts
@@ -4,6 +4,7 @@ const data: Record<string, SpiritsData> = {
   lumberjack: {
     name: "Apologetic Lumberjack",
     type: "Regular Spirit",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/28/Forest-Spirit-Apologetic-Lumberjack.png",
     realm: "Hidden Forest",
     main: {
       description: "Apologetic Lumberjack Infographics by Ed.7",
@@ -30,10 +31,49 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Face Accessory",
+        icon: "<:ApologeticLumberjackFaceGuard:1275290202771820554>",
+        type: "Face Accessory",
+        price: "5 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "Face Accessory",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/cd/Mask05-Diamond.png/revision/latest/scale-to-width-down/400?cb=20210810231659",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        icon: "<:ApologeticLumberjackHair:1275290188339216406>",
+        type: "Hair",
+        price: "3 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "The Hair (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/df/Hair07-Short_Ponytails.png/revision/latest/scale-to-width-down/400?cb=20240122085747",
+          },
+          {
+            description: "The Hair (Side)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/5/59/Apologetic-Lumberjack-Hair-side.png/revision/latest/scale-to-width-down/400?cb=20240123061712",
+          },
+          {
+            description: "The Hair (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/73/Apologetic-Lumberjack-Hair-back.png/revision/latest/scale-to-width-down/400?cb=20240123061711",
+          },
+        ],
+      },
+    ],
   },
   prospector: {
     name: "Blushing Prospector",
     type: "Regular Spirit",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/f5/Forest-Spirit-Blushing-Prospector.png",
     realm: "Hidden Forest",
     main: {
       description: "Blushing Prospector Infographics by Ed.7",
@@ -60,9 +100,61 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Hair",
+        icon: "<:BlushingProspectorHair:1275288710170280078>",
+        type: "Hair",
+        price: "3 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "The Hair (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/21/Blushing-Prospector-Hair-front.png/revision/latest/scale-to-width-down/400?cb=20240207180756",
+          },
+          {
+            description: "The Hair (Side)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/b1/Hair32-Pigtail_Braids.png/revision/latest/scale-to-width-down/400?cb=20240129042342",
+          },
+          {
+            description: "The Hair (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/5/57/Blushing-Prospector-Hair-back.png/revision/latest/scale-to-width-down/400?cb=20240207180753",
+          },
+        ],
+      },
+      {
+        name: "Drum",
+        icon: "<:BlushingProspectorInstrument:1275288696521887755>",
+        type: "Instrument",
+        price: "5 <:regularHeart:1207793247792013474>",
+        notes: [
+          "In Harmony Hall, the Drum can be found in the left-hand room, in a corner to the left of the fireplace, with the other percussion Instruments",
+        ],
+        images: [
+          {
+            description: "The Drum",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/db/Blushing_Prospector-Drum.png/revision/latest/scale-to-width-down/400?cb=20240610173530",
+          },
+          {
+            description: "Playing the drum",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/9/95/Instrument_drum_playing.png/revision/latest/scale-to-width-down/400?cb=20240610173736",
+          },
+          {
+            description: "On player's back",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/37/Instrument-High_Drum.png/revision/latest/scale-to-width-down/400?cb=20240610173620",
+          },
+        ],
+      },
+    ],
   },
   dismayed_hunter: {
     name: "Dismayed Hunter",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/66/Forest-Spirit-Dismayed-Hunter.png",
     type: "Regular Spirit",
     realm: "Hidden Forest",
     main: {
@@ -90,9 +182,84 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Hair",
+        icon: "<:DismayedHunterHair:1275286585134288927>",
+        type: "Hair",
+        price: "5 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "The Hair (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c4/Hair17-Thick_Ponytails.png/revision/latest/scale-to-width-down/400?cb=20240125074503",
+          },
+          {
+            description: "The Hair (Side)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/67/Dismayed-Hunter-Hair-side.png/revision/latest/scale-to-width-down/400?cb=20240128041659",
+          },
+          {
+            description: "The Hair (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/f2/Dismayed-Hunter-Hair-back.png/revision/latest/scale-to-width-down/400?cb=20240128041701",
+          },
+        ],
+      },
+      {
+        name: "Cape - Tier 1",
+        icon: "<:DismayedHunterCapeLvl1:1275286548031471678>",
+        type: "Cape",
+        price: "30 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "The Cape - Tier 1 (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/76/Cape-Purple.png/revision/latest/scale-to-width-down/400?cb=20210810212828",
+          },
+          {
+            description: "The Cape - Tier 1 (Interior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/7a/Cape_purple_3.png/revision/latest/scale-to-width-down/400?cb=20210810213153",
+          },
+          {
+            description: "The Cape - Tier 1 (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/dc/Cape_purple_2.png/revision/latest/scale-to-width-down/400?cb=20210810213151",
+          },
+        ],
+      },
+      {
+        name: "Cape - Tier 2",
+        icon: "<:DismayedHunterCapeLvl2:1275286537256308796>",
+        type: "Cape",
+        price: "90 <:regularHeart:1207793247792013474>",
+        notes: [
+          "The Tier 2 Cape (and the second Wing Buff that unlocks it) are not needed to complete the Hidden Forest Constellation",
+        ],
+        images: [
+          {
+            description: "The cape - Tier 2 (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/43/Cape_purple_tier_2_front.png/revision/latest/scale-to-width-down/400?cb=20210806192700",
+          },
+          {
+            description: "The cape - Tier 2 (Interior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/0/05/Cape_purple_tier_2_inner.png/revision/latest/scale-to-width-down/400?cb=20210806192702",
+          },
+          {
+            description: "The cape - Tier 2 (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/7b/Cape_purple_tier_2_back.png/revision/latest/scale-to-width-down/400?cb=20210806192658",
+          },
+        ],
+      },
+    ],
   },
-  pioneer: {
+  hide_n_seek_pioneer: {
     name: "Hide'n'seek Pioneer",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/4f/Forest-Spirit-Hide-n-Seek-Pioneer.png",
     type: "Regular Spirit",
     realm: "Hidden Forest",
     main: {
@@ -110,9 +277,64 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Outfit",
+        icon: "<:HideNSeekPioneerOutfit:1275284864106758154>",
+        type: "Outfit",
+        price: "15 <:regularHeart:1207793247792013474>",
+        notes: [
+          "This item was added to the Friendship Tree in Patch 0.13.2 on April 1, 2021.This item was added to the Friendship Tree in Patch 0.13.2 on April 1, 2021",
+          "The Outfit (and the Wing Buff that unlocks it) are not needed to complete the Hidden Forest Constellation",
+        ],
+        images: [
+          {
+            description: "The Outfit",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/10/Mask06-small-chibi.png/revision/latest/scale-to-width-down/400?cb=20210810231631",
+          },
+        ],
+      },
+      {
+        name: "Chibi Mask",
+        icon: "<:HideNSeekPioneerMask:1275284854086434847>",
+        price: "20 <:regularHeart:1207793247792013474>",
+        type: "Mask",
+        notes: [
+          "This special Mask, which causes players wearing it to be Chibi-sized, is a popular tool for going Out of Bounds (OOB)",
+          "The Shrink Spell can also be used to temporarily mimic the Mask's effect",
+        ],
+        images: [
+          {
+            description: "The Mask",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/10/Mask06-small-chibi.png/revision/latest/scale-to-width-down/400?cb=20210810231631",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        icon: "<:HideNSeekPioneerHair:1275284840048103466>",
+        type: "Hair",
+        price: "2 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "The Hair (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/40/Hair14-Bob.png/revision/latest/scale-to-width-down/400?cb=20240203011438",
+          },
+          {
+            description: "The Hair (Side)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/b8/Hide%27n%27Seek-Pioneer-Hair-side.png/revision/latest/scale-to-width-down/400?cb=20240207183101",
+          },
+        ],
+      },
+    ],
   },
   porter: {
     name: "Pouty Porter",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/3f/Forest-Spirit-Pouty-Porter.png",
     type: "Regular Spirit",
     realm: "Hidden Forest",
     main: {
@@ -140,9 +362,85 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Hair",
+        icon: "<:PoutyPorterHair:1275282629184458846>",
+        type: "Hair",
+        price: "3 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "The Hair (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/e/e7/Hair35-One_Side_Medium.png/revision/latest/scale-to-width-down/400?cb=20240206024710",
+          },
+          {
+            description: "The Hair (Side)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/fe/Pouty-Porter-Hair-side.png/revision/latest/scale-to-width-down/400?cb=20240207190449",
+          },
+          {
+            description: "The Hair (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a7/Pouty-Porter-Hair-back.png/revision/latest/scale-to-width-down/400?cb=20240207190452",
+          },
+        ],
+      },
+      {
+        name: "Cape - Tier 1",
+        icon: "<:PoutyPorterCapeLvl1:1275282614034370662>",
+        type: "Cape",
+        price: "20 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "The Cape - Tier 1 (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/46/Cape-Teal.png/revision/latest/scale-to-width-down/400?cb=20210810194755",
+          },
+          {
+            description: "The Cape - Tier 1 (Interior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/70/Cape_teal3.png/revision/latest/scale-to-width-down/400?cb=20210810213146",
+          },
+          {
+            description: "The Cape - Tier 1 (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/2/2b/Cape_teal2.png/revision/latest/scale-to-width-down/400?cb=20210810213144",
+          },
+        ],
+      },
+      {
+        name: "Cape - Tier 2",
+        icon: "<:PoutyPorterCapeLvl2:1275282593948110974>",
+        price: "60 <:regularHeart:1207793247792013474>",
+        type: "Cape",
+        notes: [
+          "The Tier 2 Cape (and the second Wing Buff that unlocks it) are not needed to complete the Hidden Forest Constellation. These were both added to the Friendship Tree in Patch 0.12.0 on December 15, 2020",
+          "Note that while the Capes' color is very similar to the special Beta Cape and Founder's Cape, the Pouty Porter's Capes do not grant the wearer any ability to open Cape-locked Constellation Gates",
+        ],
+        images: [
+          {
+            description: "The Cape - Tier 2 (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/bc/Cape_teal_tier_2_front.png/revision/latest/scale-to-width-down/400?cb=20210806192711",
+          },
+          {
+            description: "The Cape - Tier 2 (Interior)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/d/d7/Cape_teal_tier_2_inner.png/revision/latest/scale-to-width-down/400?cb=20210806192713",
+          },
+          {
+            description: "The Cape - Tier 2 (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/f/fd/Cape_teal_tier_2_back.png/revision/latest/scale-to-width-down/400?cb=20210806192709",
+          },
+        ],
+      },
+    ],
   },
   trailblazer: {
     name: "Shivering Trailblazer",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/9/98/Forest-Spirit-Shivering-Trailblazer.png",
     type: "Regular Spirit",
     realm: "Hidden Forest",
     main: {
@@ -170,9 +468,48 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Outfit",
+        icon: "<:ShiveringTrailblazerOutfit:1275281575155601439>",
+        type: "Outfit",
+        price: "2 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "The Outfit",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/46/Legs03.png/revision/latest/scale-to-width-down/400?cb=20230626010632",
+          },
+        ],
+      },
+      {
+        name: "Hair",
+        icon: "<:ShiveringTrailblazerHair:1275281562191134731>",
+        type: "Hair",
+        price: "5 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "The Hair (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Hair08-Wispy.png/revision/latest/scale-to-width-down/400?cb=20240203104957",
+          },
+          {
+            description: "The Hair (Side)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/0/0d/Shivering-Trailblazer-Hair-side.png/revision/latest/scale-to-width-down/400?cb=20240207183102",
+          },
+          {
+            description: "The Hair (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/9/9c/Shivering-Trailblazer-Hair-back.png/revision/latest/scale-to-width-down/400?cb=20240207183058",
+          },
+        ],
+      },
+    ],
   },
   miner: {
     name: "Tearful Light Miner",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/4a/Forest-Spirit-Tearful-Light-Miner.png",
     type: "Regular Spirit",
     realm: "Hidden Forest",
     main: {
@@ -208,9 +545,35 @@ const data: Record<string, SpiritsData> = {
         },
       ],
     },
+    cosmetics: [
+      {
+        name: "Hair",
+        icon: "<:TearfulLightMinerHair:1275280658972803102>",
+        type: "Hair",
+        price: "3 <:regularHeart:1207793247792013474>",
+        images: [
+          {
+            description: "The Hair (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/e/ec/Hair38-Short_Mullet.png/revision/latest/scale-to-width-down/400?cb=20240127101457",
+          },
+          {
+            description: "The Hair (Side)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/0/0b/Tearful-Light-Miner-Hair-side.png/revision/latest/scale-to-width-down/400?cb=20240128041703",
+          },
+          {
+            description: "The Hair (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/8/83/Tearful-Light-Miner-Hair-back.png/revision/latest/scale-to-width-down/400?cb=20240128041702",
+          },
+        ],
+      },
+    ],
   },
   whale_whisperer: {
     name: "Whale Whisperer",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/3b/Forest-Spirit-Whale-Whisperer.png",
     type: "Regular Spirit",
     realm: "Hidden Forest",
     main: {
@@ -222,6 +585,15 @@ const data: Record<string, SpiritsData> = {
       icon: "<:whalewhisperer:1205914501241765918>",
       image: "Whale_Call-1.mp4",
     },
+    cosmetics: [
+      {
+        name: "Music Sheet #2",
+        type: "Music Sheet",
+        icon: "<:MusicIcon:1262323496852131882>",
+        price: "2 <:regularHeart:1207793247792013474>",
+        images: [],
+      },
+    ],
   },
 };
 
