e0272b3fb3680cf300355ff4d02f95d203fb2ba5
imnaiyar <137700126+imnaiyar@users.noreply.github.com>
2024-08-27 01:52:08 -0500

    feat: add updates category docs

--- docs/components/CollapsibleGallery.tsx
+++ docs/components/CollapsibleGallery.tsx
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { IoIosArrowDropdownCircle } from "react-icons/io";
 
 /**
- * Returns a jsx component for a collapsible text that shows provided images gallery
+ * Returns a JSX component for a collapsible text that shows a provided images gallery
  * @param options Options for the collapsible gallery
  */
 const CollapsibleGallery = ({ headingText, images }: CollapsibleGalleryOptions) => {
   const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null); // Reference to the collapsible content
+  const [contentHeight, setContentHeight] = useState(0);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      setContentHeight(isOpen ? contentRef.current.scrollHeight : 0);
+    }
+  }, [isOpen]);
 
   const toggleOpen = () => {
     setIsOpen(!isOpen);
@@ -19,10 +27,16 @@ const CollapsibleGallery = ({ headingText, images }: CollapsibleGalleryOptions) 
       <p onClick={toggleOpen} style={styles.toggle}>
         {headingText}{" "}
         <span style={{ ...styles.icon, transform: isOpen ? "rotate(180deg)" : "rotate(0deg)" }}>
-          {<IoIosArrowDropdownCircle />}
+          <IoIosArrowDropdownCircle />
         </span>
       </p>
-      {isOpen && (
+      <div
+        ref={contentRef}
+        style={{
+          ...styles.collapsibleContent,
+          maxHeight: `${contentHeight}px`,
+        }}
+      >
         <div style={styles.galleryContainer}>
           <Carousel autoPlay width="400px" showThumbs={false}>
             {images.map((image, index) => (
@@ -33,7 +47,7 @@ const CollapsibleGallery = ({ headingText, images }: CollapsibleGalleryOptions) 
             ))}
           </Carousel>
         </div>
-      )}
+      </div>
     </>
   );
 };
@@ -42,16 +56,20 @@ const styles = {
   toggle: {
     fontWeight: "bold",
     cursor: "pointer",
+    display: "flex",
+    alignItems: "center",
   },
   icon: {
     display: "inline-block",
     transition: "transform 0.3s ease",
+    marginLeft: "8px",
+  },
+  collapsibleContent: {
+    overflow: "hidden",
+    transition: "max-height 0.5s ease",
   },
   galleryContainer: {
     padding: "10px",
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
   },
 };
 
