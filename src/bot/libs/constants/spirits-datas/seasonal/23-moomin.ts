777c336814190b104829212e7682937fcdc8352f
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-10-14 14:07:38 +0530

    chore: add moomin data

---
+++ src/bot/libs/constants/spirits-datas/seasonal/23-moomin.ts
@@ -0,0 +1,169 @@
+import type { SpiritsData } from "../type.js";
+
+const data: Record<string, SpiritsData> = {
+  kindness: {
+    name: "Comfort of Kindness",
+    season: "Moomin",
+    type: "Seasonal Spirit",
+    realm: "Vault of Knowledge",
+    icon: "<:ComfortofKindnessCape:1295293399406084147>",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/1/18/Comfort-of-Kindness-Shop.png",
+    ts: {
+      eligible: false,
+      returned: false,
+      dates: [],
+    },
+    tree: {
+      by: "𝓈𝓎𝓊𝒶𝓃 (syuan_syuan_) ",
+      total: "101 <:MoominCandles:1295299227332640858>",
+      image:
+        "https://cdn.discordapp.com/attachments/1295233556058210406/1295285443830480937/Image_3.jpeg?ex=670e1813&is=670cc693&hm=0b15a3e4da4a7b2e89ee7ebc83dced0ceeb687ff0517b1483e60aa883e24fb41&",
+    },
+  },
+  sense_of_self: {
+    name: "Sense of Self",
+    type: "Seasonal Spirit",
+    season: "Moomin",
+    realm: "Vault of Knowledge",
+    icon: "<:SenseofSelfNeckAccessory:1295294042258673718>",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/9/92/Sense-of-Self-Shop.png",
+    ts: {
+      eligible: false,
+      returned: false,
+      dates: [],
+    },
+    tree: {
+      by: "𝓈𝓎𝓊𝒶𝓃 (syuan_syuan_) ",
+      total: "85 <:MoominCandles:1295299227332640858>",
+      image:
+        "https://media.discordapp.net/attachments/1295233556058210406/1295285444333928499/Image_4.jpeg?ex=670e1813&is=670cc693&hm=bed55aa166fb92cb7010b681fb3c756354d3cc440477b81e326aca210842a15e&=&format=webp&width=1091&height=1091",
+    },
+  },
+  spirit_of_adventure: {
+    name: "Spirit of Adventure",
+    type: "Seasonal Spirit",
+    season: "Moomin",
+    realm: "Vault of Knowledge",
+    icon: "<:SpiritofAdventureCape:1295294188375511082>",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/c/c6/Spirit-of-Adventure-Shop.png",
+    ts: {
+      eligible: false,
+      returned: false,
+      dates: [],
+    },
+    tree: {
+      by: "𝓈𝓎𝓊𝒶𝓃 (syuan_syuan_) ",
+      total: "95 <:MoominCandles:1295299227332640858>",
+      image:
+        "https://media.discordapp.net/attachments/1295233556058210406/1295285444879323187/Image_5.jpeg?ex=670e1813&is=670cc693&hm=b347ee716d62d11b92170eb85113d908498044c1475394fc134668f56d17d5c6&=&format=webp&width=1091&height=1091",
+    },
+  },
+  inspiration_of_inclusion: {
+    name: "Inspiration of Inclusion",
+    type: "Seasonal Spirit",
+    season: "Moomin",
+    realm: "Vault of Knowledge",
+    icon: "<:InspirationofInclusionNeckAccess:1295294371876573325>",
+    image: "https://static.wikia.nocookie.net/sky-children-of-the-light/images/7/77/Inspiration-of-Inclusion-Shop.png",
+    ts: {
+      eligible: false,
+      returned: false,
+      dates: [],
+    },
+    tree: {
+      by: "𝓈𝓎𝓊𝒶𝓃 (syuan_syuan_) ",
+      total: "113 <:MoominCandles:1295299227332640858>",
+      image:
+        "https://media.discordapp.net/attachments/1295233556058210406/1295285445621448715/Image_6.jpeg?ex=670e1813&is=670cc693&hm=f6a269df85a6f8cbdfe9e00f90343b7c2b872b5b746b884a7262914e59974c78&=&format=webp&width=1091&height=1091",
+    },
+    collectibles: [
+      {
+        name: "Outfit",
+        type: "Outfit",
+        icon: "<:InspirationofInclusionOutfit:1295300513868419148>",
+        spPrice: "36 <:MoominCandles:1295299227332640858>",
+        images: [
+          {
+            description: "The Outfit, (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/4/44/Inspiration-of-Inclusion-Outfit-front.png/revision/latest/scale-to-width-down/400?cb=20240927233639",
+          },
+          {
+            description: "The Outfit, (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/e/e9/Inspiration-of-Inclusion-Outfit-back.png/revision/latest/scale-to-width-down/400?cb=20240927233641",
+          },
+        ],
+      },
+      {
+        name: "Scarf",
+        type: "Neck Accessory",
+        icon: "<:InspirationofInclusionNeckAccess:1295294371876573325>",
+        isSP: true,
+        images: [
+          {
+            description: "The Scarf (Front)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a1/Inspiration-of-Inclusion-Neck-Accessory-front.png/revision/latest/scale-to-width-down/400?cb=20240927002652",
+          },
+          {
+            description: "The Scarf (Back)",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/a/a8/Inspiration-of-Inclusion-Neck-Accessory-back.png/revision/latest/scale-to-width-down/400?cb=20240927002653",
+          },
+        ],
+      },
+      {
+        name: "Hair Accessory",
+        type: "Hair Accessory",
+        icon: "<:InclusionHairAccessory:1295300872837922886>",
+        isSP: true,
+        images: [
+          {
+            description: "The Accessory",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/b/b1/Inspiration-of-Inclusion-Hair-Accessory.png/revision/latest/scale-to-width-down/400?cb=20241002232709",
+          },
+        ],
+      },
+      {
+        name: "Grandfather Clock",
+        type: "Props",
+        icon: "<:InspirationofInclusionClockPropi:1295301238304538695>",
+        spPrice: "12 <:MoominCandles:1295299227332640858>",
+        images: [
+          {
+            description: "The Clock",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/3/38/Inspiration-of-Inclusion-Clock-Prop.png/revision/latest/scale-to-width-down/400?cb=20240926041201",
+          },
+          {
+            description: "On player's back",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/5/59/Duets-Poster-Props-Backpack.png/revision/latest/scale-to-width-down/400?cb=20240714065406",
+          },
+        ],
+      },
+      {
+        name: "Moomin Paintings",
+        type: "Props",
+        icon: "<:InspirationofInclusionPainting:1295301282659045447>",
+        spPrice: "20 <:MoominCandles:1295299227332640858>",
+        images: [
+          {
+            description: "The Painting",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/6/65/Inspiration-of-Inclusion-Painting-Prop.png/revision/latest/scale-to-width-down/400?cb=20240927230149",
+          },
+          {
+            description: "On player's back",
+            image:
+              "https://static.wikia.nocookie.net/sky-children-of-the-light/images/5/59/Duets-Poster-Props-Backpack.png/revision/latest/scale-to-width-down/400?cb=20240714065406",
+          },
+        ],
+      },
+    ],
+  },
+};
+
+export default data;
