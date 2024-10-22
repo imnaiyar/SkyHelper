6de0c698d0a5d2de9d107701776d05a1c07bb4ac
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-10-22 16:54:38 +0530

    chore: add env validation

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -7,7 +7,17 @@ import * as Sentry from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 import chalk from "chalk";
-console.log(chalk.blueBright("\n\n<------------------------ Initiaizing Sentry --------------------------->\n"));
+import { validateEnv } from "./utils/validators.js";
+import { CustomLogger } from "./handlers/logger.js";
+
+// validate env
+console.log(chalk.blueBright(`\n\n<${"-".repeat(26)} Validating Env ${"-".repeat(26)}>\n`));
+try {
+  validateEnv();
+} catch (e: any) {
+  CustomLogger.log({ level: { name: "ERROR", emoji: "☠", color: "\x1b[31m" } }, e.message);
+  process.exit(1);
+}
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
@@ -22,7 +32,9 @@ Sentry.init({
   tracesSampleRate: 1.0,
   profilesSampleRate: 1.0,
 });
-console.log("Sentry Initialized");
+
+console.log("\n\n");
+CustomLogger.log({ level: { name: "Sentry", color: "\x1b[36m" } }, "Sentry Initialized\n\n\n");
 
 const root = process.isBun ? "src/bot" : "dist/bot";
 await client.loadEvents(root + "/events");
@@ -30,7 +42,7 @@ await client.loadCommands(root + "/commands/inputCommands");
 await client.loadContextCmd(root + "/commands/contexts");
 await client.loadButtons(root + "/buttons");
 await initializeMongoose();
-console.log(chalk.blueBright("\n\n<------------------------ Dashboard --------------------------->\n"));
+console.log(chalk.blueBright(`\n\n<${"-".repeat(24)} Dashboard ${"-".repeat(26)}>\n`));
 if (client.config.DASHBOARD.enabled && process.env.NODE_ENV !== "development") Dashboard(client);
 // Catching unhandle rejections
 process.on("unhandledRejection", client.logger.error);
---
+++ src/bot/utils/validators.ts
@@ -0,0 +1,12 @@
+import logger from "#bot/handlers/logger";
+
+export function validateEnv() {
+  if (!process.env.TOKEN) throw new Error("TOKEN is not provided in the environment variables");
+  if (!process.env.MONGO_CONNECTION) throw new Error("MONGO_CONNECTION is not provided in the environment variables");
+  if (!process.env.SENTRY_DSN) throw new Error("SENTRY_DSN is not provided in the environment variables");
+  if (!process.env.CONTACT_US) logger.warn("CONTACT_US Webhook is not provided, command '/contact-us' will not work properly");
+  for (const key of ["TOPGG_TOKEN", "DBL_TOKEN"]) {
+    // prettier-ignore
+    if (!process.env[key]) logger.warn(`${key} is not provided in the environment variables, the bot will not be able to post stats`);
+  }
+}
