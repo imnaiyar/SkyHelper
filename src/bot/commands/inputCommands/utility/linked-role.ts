015faf9e375affe4303a78a1eeccc4374a2c722b
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2024-12-22 02:21:15 +0530

    some info

--- src/bot/commands/inputCommands/utility/linked-role.ts
+++ src/bot/commands/inputCommands/utility/linked-role.ts
@@ -8,9 +8,12 @@ export default {
     const text = `Linked role is a special kind of roles that can be used to connect an user account with an application (in this case, the SkyHelper bot).
 This allows the bot to update user's role connection data to display specific information on a user's profile in a server (see the attached pic). In this case, we can use this to display basic sky profile (Preferred name, preferences, etcS).
 Due to the complicated nature of linked roles, it is difficult to explain everything here, so we have prepared some guides to help you understand how to use linked roles.
-- [What is a linked role and how you can get them?](https://docs.skyhelper.xyz/linked-role/about)
-- [How you can enable linked roles in your server?](https://docs.skyhelper.xyz/linked-role/how-to-use)
-- [How to link/update your sky profile with the bot?](https://docs.skyhelper.xyz/linked-role/how-to-link)`;
+- [What is a linked?](https://docs.skyhelper.xyz/linked-role/about)
+- [How you can enable linked roles in your server?](https://docs.skyhelper.xyz/linked-role/how-to-enable)
+- [How to get/link/update your sky profile with the bot?](https://docs.skyhelper.xyz/linked-role/how-to-link)
+
+-# - If Linked Role connection is enabled for SkyHelper in this server, you can get it by navigating to <id:linked-role>.`;
+
     await interaction.reply({ content: text, ephemeral });
   },
 } satisfies Command;
