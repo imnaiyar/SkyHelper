45294ea877a0a503d668df67a9097b156f924644
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2025-01-21 03:19:43 +0530

    feat: resolveColors

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,7 +1,6 @@
-import { buildTimesHTML, parsePerms, postToHaste, recursiveReadDir, type Field, parseDateFormat } from "./utils/index.js";
+export * from "./utils/index.js";
 export * from "./classes/index.js";
 export { type Permission } from "./utils/parsePerms.js";
-export { buildTimesHTML, parsePerms, postToHaste, parseDateFormat, type Field, recursiveReadDir };
 export * from "./constants/index.js";
 // Export types for docs
 export type * from "./typings.ts";
--- packages/utils/src/utils/index.ts
+++ packages/utils/src/utils/index.ts
@@ -3,3 +3,4 @@ export { postToHaste } from "./postToBin.js";
 export { parsePerms, type Permission } from "./parsePerms.js";
 export { recursiveReadDir } from "./recursiveReadDir.js";
 export { parseDateFormat } from "./parseDateFormat.js";
+export { resolveColor } from "./resolveColors.js";
