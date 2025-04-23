5e59b347037c3e7e4aae56267c47660faf9ba217
imnaiyar <imamnaiyar29@gmail.com>
2025-04-23 12:48:44 +0600

    fix(ShardsScroll): missing v2 flag

--- packages/skyhelper/src/bot/modules/buttons/shards-scroll.ts
+++ packages/skyhelper/src/bot/modules/buttons/shards-scroll.ts
@@ -2,6 +2,7 @@ import { buildShardEmbed } from "@/utils/classes/Embeds";
 import type { Button } from "@/structures";
 import { ShardsUtil as utils } from "@skyhelperbot/utils";
 import type { DateTime } from "luxon";
+import { MessageFlags } from "@discordjs/core";
 export default {
   data: {
     name: "shards-scroll",
@@ -11,6 +12,7 @@ export default {
     const givenDate = utils.getDate(date) as DateTime;
     await helper.deferUpdate();
     const res = buildShardEmbed(givenDate, t);
-    await helper.editReply(res);
+
+    await helper.editReply({ ...res, flags: MessageFlags.IsComponentsV2 });
   },
 } satisfies Button;
