7bac1ae15de69a5c02ac16b931f7a79d1a3eb661
imnaiyar <imamnaiyar29@gmail.com>
2025-05-23 14:54:22 +0600

    chore: v2 comp for no data daily quest response

--- packages/skyhelper/src/bot/modules/inputCommands/info/daily-quests.ts
+++ packages/skyhelper/src/bot/modules/inputCommands/info/daily-quests.ts
@@ -1,6 +1,7 @@
 import { dailyQuestEmbed } from "@/utils/classes/Embeds";
 import type { Command, SkyHelper } from "@/structures";
 import { DateTime } from "luxon";
+import { textDisplay, separator } from "@skyhelperbot/utils";
 
 import type { getTranslator } from "@/i18n";
 import { DAILY_QUESTS_DATA } from "@/modules/commands-data/info-commands";
@@ -19,7 +20,16 @@ const getQuestResponse = async (client: SkyHelper, t: ReturnType<typeof getTrans
   const now = DateTime.now().setZone(client.timezone).startOf("day");
   const lastUpdated = DateTime.fromISO(data.last_updated, { zone: client.timezone }).startOf("day");
   if (!data.last_updated || !now.equals(lastUpdated) || !data.quests.length) {
-    return { content: t("commands:DAILY_QUESTS.RESPONSES.NO_DATA") };
+    return {
+      components: [
+        textDisplay("No data found for today's daily quests. Please try again later."),
+        separator(true, 1),
+        textDisplay(
+          "-# Please note that it can take upto 30min-1hrs after daily resets for quests to be updated. The time frame given is just an estimate, on somedays, it can take longer than that. See how it works [here](<https://docs.skyhelper.xyz/commands/info#how-does-this-work>)",
+        ),
+      ],
+      flags: MessageFlags.IsComponentsV2,
+    };
   }
   return { ...dailyQuestEmbed(data), flags: MessageFlags.IsComponentsV2 };
 };
