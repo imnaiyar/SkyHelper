af66034f25834513b13dfa6006e002cbd73c3ffe
imnaiyar <137700126+imnaiyar@users.noreply.github.com.>
2025-08-21 14:25:34 +0530

    chore: some changes

--- apps/docs-v2/scripts/generate-docs.mjs
+++ apps/docs-v2/scripts/generate-docs.mjs
@@ -2,7 +2,11 @@ import { generateFiles } from "fumadocs-openapi";
 
 void generateFiles({
   // the OpenAPI schema, you can also give it an external URL.
-  input: ["./openapi.json"],
+  input: [
+    process.env.NODE_ENV === "development"
+      ? "../../packages/skyhelper/openapi.json"
+      : "https://raw.githubusercontent.com/imnaiyar/skyhelper-openapi-spec/refs/heads/main/openapi.json",
+  ],
   output: "./content/docs/openapi",
   // we recommend to enable it
   // make sure your endpoint description doesn't break MDX syntax.
