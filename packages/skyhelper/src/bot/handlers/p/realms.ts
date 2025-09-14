1d296b48b63e191acdf1eca64d22d641ce80e39e
imnaiyar <137700126+imnaiyar@users.noreply.github.com.>
2025-09-14 16:06:04 +0530

    feat: items, relams display

---
+++ packages/skyhelper/src/bot/handlers/p/realms.ts
@@ -0,0 +1,35 @@
+import { container, section, textDisplay } from "@skyhelperbot/utils";
+import { BasePlannerHandler, DisplayTabs } from "./base.js";
+import { ComponentType } from "discord-api-types/v10";
+import { CustomId, store } from "@/utils/customId-store";
+
+export class RealmsDisplay extends BasePlannerHandler {
+  handle() {
+    return { components: [this.realmslist()] };
+  }
+  realmslist() {
+    const realms = this.data.realms;
+    return container(
+      this.createTopCategoryRow(DisplayTabs.Realms, this.state.user),
+      textDisplay(`# Realms (${realms.length})`),
+      ...this.displayPaginatedList({
+        items: realms,
+        user: this.state.user,
+        tab: this.state.tab,
+        perpage: 7,
+        itemCallback: (realm) => [
+          section(
+            {
+              type: ComponentType.Button,
+              label: "View",
+              custom_id: this.createCustomId(DisplayTabs.Realms, this.state.user, undefined, realm.guid),
+              style: 1,
+            },
+            `**${realm.name}**`,
+            `${realm.areas?.length || 0} Areas • ${this.planner.getSpiritsInRealm(realm.guid, this.data).length} Spirits • ${this.planner.getWingedLightsInRealm(realm.guid, this.data).length} Winged Light`,
+          ),
+        ],
+      }),
+    );
+  }
+}
