601573c3cbf61261259f124daeb9cdf63c192556
Naiyar <137700126+imnaiyar@users.noreply.github.com>
2025-10-07 18:16:06 +0530

    chore: rewrite to docs url in main website (#341)

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -5,6 +5,7 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  assetPrefix: "/docs-static",
 };
 
 export default withMDX(config);
