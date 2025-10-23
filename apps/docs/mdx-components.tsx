165e79446975c87590b878b8085c183f11518ab5
imnaiyar <137700126+imnaiyar@users.noreply.github.com.>
2025-10-24 01:34:19 +0530

    chore: sync

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -2,12 +2,13 @@ import { APIPage } from "fumadocs-openapi/ui";
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import type { MDXComponents } from "mdx/types";
 import { openapi } from "./lib/source";
-
+import Collapsible from "./app/components/collapsible";
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     APIPage: (props) => <APIPage {...openapi.getAPIPageProps(props)} />,
     ...components,
+    Collapsible,
   };
 }
