f340ab001e36a36c5f861f4ed7b8cfe1a2cafb01
imnaiyar <137700126+imnaiyar@users.noreply.github.com.>
2025-11-02 19:00:30 +0530

    chore: lint

--- apps/website/eslint.config.mjs
+++ apps/website/eslint.config.mjs
@@ -15,6 +15,7 @@ const eslintConfig = [
     ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"],
     rules: {
       "react/no-unescaped-entities": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
     },
   },
 ];
